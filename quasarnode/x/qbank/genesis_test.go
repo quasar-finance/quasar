6ab05e5cb3499477429013729adda960c9181389
akure <abhi.california@gmail.com>
2021-12-30 13:34:57 +0530

    Scaffolded qbank module

---
+++ quasarnode/x/qbank/genesis_test.go
@@ -0,0 +1,29 @@
+package qbank_test
+
+import (
+	"testing"
+
+	keepertest "github.com/abag/quasarnode/testutil/keeper"
+	"github.com/abag/quasarnode/testutil/nullify"
+	"github.com/abag/quasarnode/x/qbank"
+	"github.com/abag/quasarnode/x/qbank/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.QbankKeeper(t)
+	qbank.InitGenesis(ctx, *k, genesisState)
+	got := qbank.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
