8de0525463d9126ce5a5c401410ad73c258caa82
akure <abhi.california@gmail.com>
2022-03-06 21:53:35 +0530

    Added orion module documents and fixes

---
+++ quasarnode/x/osmolpv/spec/02_state.md
@@ -0,0 +1,7 @@
+# State 
+
+## Strategy Names 
+## Strategy Positons 
+## Module account balances
+## Fee collection
+## Rewards 
--- quasarnode/x/osmolpv/types/strategies.go
+++ quasarnode/x/osmolpv/types/strategies.go
@@ -14,24 +14,19 @@ const (
 	Meissa3m      = "Meissa.3m"
 )
 
+// Strategy name to lockup period mapping
 var MeissaStrategiesLockup = map[string]qbanktypes.LockupTypes{
 	MeissaInvalid: qbanktypes.LockupTypes_Invalid,
 	Meissa7d:      qbanktypes.LockupTypes_Days_7,
 	Meissa21d:     qbanktypes.LockupTypes_Days_21,
 	Meissa1m:      qbanktypes.LockupTypes_Months_1,
 	Meissa3m:      qbanktypes.LockupTypes_Months_3,
-	/*
-		"Invalid":    qbanktypes.LockupTypes_Invalid,
-		"Meissa.7d":  qbanktypes.LockupTypes_Days_7,
-		"Meissa.21d": qbanktypes.LockupTypes_Days_21,
-		"Meissa.1m":  qbanktypes.LockupTypes_Months_1,
-		"Meissa.3m":  qbanktypes.LockupTypes_Months_3,
-	*/
 	//"Meissa.6m":  banktypes.LockupTypes_Months_6,
 	//"Meissa.9m":  banktypes.LockupTypes_Months_9,
 	//"Meissa.12m":  banktypes.LockupTypes_Months_12,
 }
 
+// Lockup type to strategy name mapping
 var LockupMeissaStrategies = map[qbanktypes.LockupTypes]string{
 
 	qbanktypes.LockupTypes_Invalid:  MeissaInvalid,
