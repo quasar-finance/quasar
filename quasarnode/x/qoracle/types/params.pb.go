b62a301fb78531ff7e35e91017920f801dae7a76
akure <abhi.california@gmail.com>
2022-03-30 20:50:59 +0530

    Added one hop ibc denom map between quasar and osmo

--- quasarnode/x/qoracle/types/params.pb.go
+++ quasarnode/x/qoracle/types/params.pb.go
@@ -23,16 +23,77 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
+type OneHopIbcDenomMapping struct {
+	OriginName string `protobuf:"bytes,1,opt,name=originName,proto3" json:"originName,omitempty" yaml:"origin_name"`
+	Quasar     string `protobuf:"bytes,2,opt,name=quasar,proto3" json:"quasar,omitempty" yaml:"quasar"`
+	Osmo       string `protobuf:"bytes,3,opt,name=osmo,proto3" json:"osmo,omitempty" yaml:"osmo"`
+}
+
+func (m *OneHopIbcDenomMapping) Reset()         { *m = OneHopIbcDenomMapping{} }
+func (m *OneHopIbcDenomMapping) String() string { return proto.CompactTextString(m) }
+func (*OneHopIbcDenomMapping) ProtoMessage()    {}
+func (*OneHopIbcDenomMapping) Descriptor() ([]byte, []int) {
+	return fileDescriptor_95f4338228c132f2, []int{0}
+}
+func (m *OneHopIbcDenomMapping) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *OneHopIbcDenomMapping) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_OneHopIbcDenomMapping.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *OneHopIbcDenomMapping) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_OneHopIbcDenomMapping.Merge(m, src)
+}
+func (m *OneHopIbcDenomMapping) XXX_Size() int {
+	return m.Size()
+}
+func (m *OneHopIbcDenomMapping) XXX_DiscardUnknown() {
+	xxx_messageInfo_OneHopIbcDenomMapping.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_OneHopIbcDenomMapping proto.InternalMessageInfo
+
+func (m *OneHopIbcDenomMapping) GetOriginName() string {
+	if m != nil {
+		return m.OriginName
+	}
+	return ""
+}
+
+func (m *OneHopIbcDenomMapping) GetQuasar() string {
+	if m != nil {
+		return m.Quasar
+	}
+	return ""
+}
+
+func (m *OneHopIbcDenomMapping) GetOsmo() string {
+	if m != nil {
+		return m.Osmo
+	}
+	return ""
+}
+
 // Params defines the parameters for the module.
 type Params struct {
-	OracleAccounts string   `protobuf:"bytes,1,opt,name=oracleAccounts,proto3" json:"oracleAccounts,omitempty" yaml:"oracle_accounts"`
-	StableDenoms   []string `protobuf:"bytes,2,rep,name=stableDenoms,proto3" json:"stableDenoms,omitempty" yaml:"stable_denoms"`
+	OracleAccounts string                   `protobuf:"bytes,1,opt,name=oracleAccounts,proto3" json:"oracleAccounts,omitempty" yaml:"oracle_accounts"`
+	StableDenoms   []string                 `protobuf:"bytes,2,rep,name=stableDenoms,proto3" json:"stableDenoms,omitempty" yaml:"stable_denoms"`
+	OneHopDenomMap []*OneHopIbcDenomMapping `protobuf:"bytes,3,rep,name=oneHopDenomMap,proto3" json:"oneHopDenomMap,omitempty" yaml:"onehop_ibcdenoms"`
 }
 
 func (m *Params) Reset()      { *m = Params{} }
 func (*Params) ProtoMessage() {}
 func (*Params) Descriptor() ([]byte, []int) {
-	return fileDescriptor_95f4338228c132f2, []int{0}
+	return fileDescriptor_95f4338228c132f2, []int{1}
 }
 func (m *Params) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -75,29 +136,91 @@ func (m *Params) GetStableDenoms() []string {
 	return nil
 }
 
+func (m *Params) GetOneHopDenomMap() []*OneHopIbcDenomMapping {
+	if m != nil {
+		return m.OneHopDenomMap
+	}
+	return nil
+}
+
 func init() {
+	proto.RegisterType((*OneHopIbcDenomMapping)(nil), "abag.quasarnode.qoracle.OneHopIbcDenomMapping")
 	proto.RegisterType((*Params)(nil), "abag.quasarnode.qoracle.Params")
 }
 
 func init() { proto.RegisterFile("qoracle/params.proto", fileDescriptor_95f4338228c132f2) }
 
 var fileDescriptor_95f4338228c132f2 = []byte{
-	// 237 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x29, 0xcc, 0x2f, 0x4a,
-	0x4c, 0xce, 0x49, 0xd5, 0x2f, 0x48, 0x2c, 0x4a, 0xcc, 0x2d, 0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9,
-	0x17, 0x12, 0x4f, 0x4c, 0x4a, 0x4c, 0xd7, 0x2b, 0x2c, 0x4d, 0x2c, 0x4e, 0x2c, 0xca, 0xcb, 0x4f,
-	0x49, 0xd5, 0x83, 0xaa, 0x92, 0x12, 0x49, 0xcf, 0x4f, 0xcf, 0x07, 0xab, 0xd1, 0x07, 0xb1, 0x20,
-	0xca, 0x95, 0x26, 0x30, 0x72, 0xb1, 0x05, 0x80, 0xf5, 0x0b, 0x39, 0x71, 0xf1, 0x41, 0x94, 0x3a,
-	0x26, 0x27, 0xe7, 0x97, 0xe6, 0x95, 0x14, 0x4b, 0x30, 0x2a, 0x30, 0x6a, 0x70, 0x3a, 0x49, 0x7d,
-	0xba, 0x27, 0x2f, 0x56, 0x99, 0x98, 0x9b, 0x63, 0xa5, 0x04, 0x91, 0x8f, 0x4f, 0x84, 0x2a, 0x50,
-	0x0a, 0x42, 0xd3, 0x21, 0x64, 0xc3, 0xc5, 0x53, 0x5c, 0x92, 0x98, 0x94, 0x93, 0xea, 0x92, 0x9a,
-	0x97, 0x9f, 0x5b, 0x2c, 0xc1, 0xa4, 0xc0, 0xac, 0xc1, 0xe9, 0x24, 0xf1, 0xe9, 0x9e, 0xbc, 0x08,
-	0xc4, 0x04, 0x88, 0x6c, 0x7c, 0x0a, 0x58, 0x5a, 0x29, 0x08, 0x45, 0xb5, 0x15, 0xcb, 0x8c, 0x05,
-	0xf2, 0x0c, 0x4e, 0x2e, 0x27, 0x1e, 0xc9, 0x31, 0x5e, 0x78, 0x24, 0xc7, 0xf8, 0xe0, 0x91, 0x1c,
-	0xe3, 0x84, 0xc7, 0x72, 0x0c, 0x17, 0x1e, 0xcb, 0x31, 0xdc, 0x78, 0x2c, 0xc7, 0x10, 0xa5, 0x95,
-	0x9e, 0x59, 0x92, 0x51, 0x9a, 0xa4, 0x97, 0x9c, 0x9f, 0xab, 0x0f, 0xf2, 0xa6, 0x3e, 0xc2, 0x9b,
-	0xfa, 0x15, 0xfa, 0xb0, 0xe0, 0x28, 0xa9, 0x2c, 0x48, 0x2d, 0x4e, 0x62, 0x03, 0xfb, 0xcf, 0x18,
-	0x10, 0x00, 0x00, 0xff, 0xff, 0xa2, 0xa6, 0xeb, 0x3d, 0x26, 0x01, 0x00, 0x00,
+	// 386 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x92, 0xbf, 0xaa, 0xdb, 0x30,
+	0x14, 0xc6, 0xed, 0x38, 0x04, 0xa2, 0xb4, 0x29, 0x15, 0x69, 0x62, 0x52, 0xb0, 0x83, 0xba, 0xa4,
+	0x1d, 0x6c, 0x68, 0xa1, 0x43, 0xe8, 0x52, 0x93, 0xa1, 0x1d, 0xfa, 0x07, 0x8f, 0x5d, 0x8c, 0xec,
+	0x08, 0xc7, 0x10, 0xe9, 0x38, 0x96, 0x03, 0xcd, 0x03, 0x74, 0xef, 0xd8, 0xad, 0x7d, 0x9c, 0x8e,
+	0x19, 0xef, 0x64, 0x2e, 0xc9, 0x1b, 0xf8, 0x09, 0x2e, 0x91, 0x6c, 0xee, 0xbd, 0xe1, 0xde, 0x4d,
+	0xe8, 0xfb, 0x9d, 0x73, 0xbe, 0xf3, 0x49, 0x68, 0xb4, 0x85, 0x82, 0x26, 0x1b, 0xe6, 0xe7, 0xb4,
+	0xa0, 0x5c, 0x7a, 0x79, 0x01, 0x25, 0xe0, 0x09, 0x8d, 0x69, 0xea, 0x6d, 0x77, 0x54, 0xd2, 0x42,
+	0xc0, 0x8a, 0x79, 0x0d, 0x35, 0x1d, 0xa5, 0x90, 0x82, 0x62, 0xfc, 0xf3, 0x49, 0xe3, 0xe4, 0xaf,
+	0x89, 0x5e, 0x7c, 0x13, 0xec, 0x13, 0xe4, 0x9f, 0xe3, 0x64, 0xc9, 0x04, 0xf0, 0x2f, 0x34, 0xcf,
+	0x33, 0x91, 0xe2, 0xf7, 0x08, 0x41, 0x91, 0xa5, 0x99, 0xf8, 0x4a, 0x39, 0xb3, 0xcd, 0x99, 0x39,
+	0xef, 0x07, 0xe3, 0xba, 0x72, 0xf1, 0x9e, 0xf2, 0xcd, 0x82, 0x68, 0x2d, 0x12, 0x94, 0x33, 0x12,
+	0xde, 0x21, 0xf1, 0x6b, 0xd4, 0xd3, 0xd3, 0xed, 0x8e, 0xaa, 0x79, 0x5e, 0x57, 0xee, 0x53, 0x5d,
+	0xa3, 0xef, 0x49, 0xd8, 0x00, 0xf8, 0x15, 0xea, 0x82, 0xe4, 0x60, 0x5b, 0x0a, 0x7c, 0x56, 0x57,
+	0xee, 0xa0, 0x69, 0x2e, 0x39, 0x90, 0x50, 0x89, 0xe4, 0x57, 0x07, 0xf5, 0xbe, 0xab, 0x0d, 0x71,
+	0x80, 0x86, 0x7a, 0x99, 0x8f, 0x49, 0x02, 0x3b, 0x51, 0xca, 0xc6, 0xd6, 0xb4, 0xae, 0xdc, 0x71,
+	0x6b, 0xeb, 0xac, 0x47, 0xb4, 0x01, 0x48, 0x78, 0x51, 0x81, 0x3f, 0xa0, 0x27, 0xb2, 0xa4, 0xf1,
+	0x86, 0xa9, 0x65, 0xa5, 0xdd, 0x99, 0x59, 0xf3, 0x7e, 0x60, 0xd7, 0x95, 0x3b, 0xd2, 0x1d, 0xb4,
+	0x1a, 0xad, 0x94, 0x4c, 0xc2, 0x7b, 0x34, 0x96, 0x68, 0x08, 0x2a, 0xad, 0x36, 0x2a, 0xdb, 0x9a,
+	0x59, 0xf3, 0xc1, 0x5b, 0xcf, 0x7b, 0x24, 0x76, 0xef, 0xc1, 0x70, 0x83, 0x97, 0x75, 0xe5, 0x4e,
+	0x1a, 0xc7, 0x82, 0xad, 0x21, 0x8f, 0xb2, 0x38, 0x69, 0x47, 0x5e, 0x8c, 0x58, 0x74, 0xff, 0xfc,
+	0x73, 0x8d, 0x60, 0xf9, 0xff, 0xe8, 0x98, 0x87, 0xa3, 0x63, 0x5e, 0x1f, 0x1d, 0xf3, 0xf7, 0xc9,
+	0x31, 0x0e, 0x27, 0xc7, 0xb8, 0x3a, 0x39, 0xc6, 0x8f, 0x37, 0x69, 0x56, 0xae, 0x77, 0xb1, 0x97,
+	0x00, 0xf7, 0xcf, 0x36, 0xfc, 0x5b, 0x1b, 0xfe, 0x4f, 0xbf, 0xfd, 0x25, 0xe5, 0x3e, 0x67, 0x32,
+	0xee, 0xa9, 0x67, 0x7f, 0x77, 0x13, 0x00, 0x00, 0xff, 0xff, 0x5e, 0xee, 0x93, 0x8f, 0x3d, 0x02,
+	0x00, 0x00,
+}
+
+func (m *OneHopIbcDenomMapping) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *OneHopIbcDenomMapping) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *OneHopIbcDenomMapping) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.Osmo) > 0 {
+		i -= len(m.Osmo)
+		copy(dAtA[i:], m.Osmo)
+		i = encodeVarintParams(dAtA, i, uint64(len(m.Osmo)))
+		i--
+		dAtA[i] = 0x1a
+	}
+	if len(m.Quasar) > 0 {
+		i -= len(m.Quasar)
+		copy(dAtA[i:], m.Quasar)
+		i = encodeVarintParams(dAtA, i, uint64(len(m.Quasar)))
+		i--
+		dAtA[i] = 0x12
+	}
+	if len(m.OriginName) > 0 {
+		i -= len(m.OriginName)
+		copy(dAtA[i:], m.OriginName)
+		i = encodeVarintParams(dAtA, i, uint64(len(m.OriginName)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
 }
 
 func (m *Params) Marshal() (dAtA []byte, err error) {
@@ -120,6 +243,20 @@ func (m *Params) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
+	if len(m.OneHopDenomMap) > 0 {
+		for iNdEx := len(m.OneHopDenomMap) - 1; iNdEx >= 0; iNdEx-- {
+			{
+				size, err := m.OneHopDenomMap[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				if err != nil {
+					return 0, err
+				}
+				i -= size
+				i = encodeVarintParams(dAtA, i, uint64(size))
+			}
+			i--
+			dAtA[i] = 0x1a
+		}
+	}
 	if len(m.StableDenoms) > 0 {
 		for iNdEx := len(m.StableDenoms) - 1; iNdEx >= 0; iNdEx-- {
 			i -= len(m.StableDenoms[iNdEx])
@@ -150,6 +287,27 @@ func encodeVarintParams(dAtA []byte, offset int, v uint64) int {
 	dAtA[offset] = uint8(v)
 	return base
 }
+func (m *OneHopIbcDenomMapping) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.OriginName)
+	if l > 0 {
+		n += 1 + l + sovParams(uint64(l))
+	}
+	l = len(m.Quasar)
+	if l > 0 {
+		n += 1 + l + sovParams(uint64(l))
+	}
+	l = len(m.Osmo)
+	if l > 0 {
+		n += 1 + l + sovParams(uint64(l))
+	}
+	return n
+}
+
 func (m *Params) Size() (n int) {
 	if m == nil {
 		return 0
@@ -166,6 +324,12 @@ func (m *Params) Size() (n int) {
 			n += 1 + l + sovParams(uint64(l))
 		}
 	}
+	if len(m.OneHopDenomMap) > 0 {
+		for _, e := range m.OneHopDenomMap {
+			l = e.Size()
+			n += 1 + l + sovParams(uint64(l))
+		}
+	}
 	return n
 }
 
@@ -174,6 +338,152 @@ func sovParams(x uint64) (n int) {
 }
 func sozParams(x uint64) (n int) {
 	return sovParams(uint64((x << 1) ^ uint64((int64(x) >> 63))))
+}
+func (m *OneHopIbcDenomMapping) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowParams
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: OneHopIbcDenomMapping: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: OneHopIbcDenomMapping: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field OriginName", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowParams
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthParams
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthParams
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.OriginName = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Quasar", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowParams
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthParams
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthParams
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Quasar = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 3:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Osmo", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowParams
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthParams
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthParams
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Osmo = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipParams(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthParams
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
 }
 func (m *Params) Unmarshal(dAtA []byte) error {
 	l := len(dAtA)
@@ -267,6 +577,40 @@ func (m *Params) Unmarshal(dAtA []byte) error {
 				return io.ErrUnexpectedEOF
 			}
 			m.StableDenoms = append(m.StableDenoms, string(dAtA[iNdEx:postIndex]))
+			iNdEx = postIndex
+		case 3:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field OneHopDenomMap", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowParams
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthParams
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthParams
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.OneHopDenomMap = append(m.OneHopDenomMap, &OneHopIbcDenomMapping{})
+			if err := m.OneHopDenomMap[len(m.OneHopDenomMap)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
