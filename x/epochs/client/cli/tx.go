27f9dcb1fd5271500916cf6c4b1f174972d8560c
Antoine <antoine@advancedblockchain.com>
2022-04-09 18:31:22 +0200

    Fix imports

--- x/epochs/client/cli/tx.go
+++ x/epochs/client/cli/tx.go
@@ -3,12 +3,9 @@ package cli
 import (
 	"fmt"
 
-	"github.com/spf13/cobra"
-
-	"github.com/cosmos/cosmos-sdk/client"
-
-	// "github.com/cosmos/cosmos-sdk/client/flags".
 	"github.com/abag/quasarnode/x/epochs/types"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/spf13/cobra"
 )
 
 // GetTxCmd returns the transaction commands for this module.
