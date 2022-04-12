762a8c911838ec09f3c3c73e5ff86710d3f98772
Ehsan-saradar <ehsan.saradar@gmail.com>
2022-04-13 00:00:04 +0430

    Replace the old intergamm module (raw ibc) with ica implementation

--- x/intergamm/keeper/keeper.go
+++ x/intergamm/keeper/keeper.go
@@ -22,14 +22,16 @@ import (
 	host "github.com/cosmos/ibc-go/v3/modules/core/24-host"
 )
 
-type Keeper struct {
-	cdc                 codec.BinaryCodec
-	storeKey            sdk.StoreKey
-	memKey              sdk.StoreKey
-	scopedKeeper        capabilitykeeper.ScopedKeeper
-	icaControllerKeeper icacontrollerkeeper.Keeper
-	paramstore          paramtypes.Subspace
-}
+type (
+	Keeper struct {
+		cdc                 codec.BinaryCodec
+		storeKey            sdk.StoreKey
+		memKey              sdk.StoreKey
+		scopedKeeper        capabilitykeeper.ScopedKeeper
+		icaControllerKeeper icacontrollerkeeper.Keeper
+		paramstore          paramtypes.Subspace
+	}
+)
 
 func NewKeeper(
 	cdc codec.BinaryCodec,
