2e04e5b7201f895f1f426b00370ce6550aa724eb
akure <abhi.california@gmail.com>
2022-04-14 17:28:27 +0530

    added param test

--- x/orion/keeper/params_test.go
+++ x/orion/keeper/params_test.go
@@ -5,6 +5,7 @@ import (
 
 	testkeeper "github.com/abag/quasarnode/testutil/keeper"
 	"github.com/abag/quasarnode/x/orion/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 )
 
@@ -13,6 +14,14 @@ func TestGetParams(t *testing.T) {
 	params := types.DefaultParams()
 
 	k.SetParams(ctx, params)
-
 	require.EqualValues(t, params, k.GetParams(ctx))
+
+	params.Enabled = false
+	params.LpEpochId = "day"
+	params.PerfFeePer = sdk.NewDecWithPrec(5, 2)
+	params.MgmtFeePer = sdk.NewDecWithPrec(3, 3)
+
+	k.SetParams(ctx, params)
+	require.EqualValues(t, params, k.GetParams(ctx))
+
 }
