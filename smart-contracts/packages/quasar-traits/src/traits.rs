f6c5150eeb270b647364522ba997f8d3949a3c97
LaurensKubat <Laurens.kubat@gmail.com>
2022-05-20 11:55:50 +0200

    add demo distributor

--- smart-contracts/packages/quasar-traits/src/traits.rs
+++ smart-contracts/packages/quasar-traits/src/traits.rs
@@ -3,5 +3,6 @@ use cosmwasm_std::{Uint128};
 
 
 pub trait ShareDistributor {
-    fn distribute_shares(&self, deposit: Vec<Cw20Coin>, state: Vec<Cw20Coin>) -> Result<Uint128, cosmwasm_std::StdError>;
+    fn deposit_funds(&mut self, deposit: &Vec<Cw20Coin>, state: &Vec<Cw20Coin>) -> Result<Uint128, cosmwasm_std::StdError>;
+    fn withdraw_funds(&mut self, shares: &Uint128, state: &Vec<Cw20Coin>) -> Result<Vec<Cw20Coin>, cosmwasm_std::StdError>;
 }
