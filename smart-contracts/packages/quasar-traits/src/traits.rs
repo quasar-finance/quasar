7eee589806c2cd54e1c195b27927057defe8f061
LaurensKubat <Laurens.kubat@gmail.com>
2022-05-20 11:56:09 +0200

    update docs

--- smart-contracts/packages/quasar-traits/src/traits.rs
+++ smart-contracts/packages/quasar-traits/src/traits.rs
@@ -2,6 +2,10 @@ use cw20::{Cw20Coin};
 use cosmwasm_std::{Uint128};
 
 
+/// ShareDistributor is the trait describing the logic behind distributing shares within a quasar vault.
+/// A share distributor does not allow for preferential treatment of certain addresses. Preferential
+/// treatment has to be done at contract level.
+/// deposit_funds() and withdraw_funds() should be reversible at the same state.
 pub trait ShareDistributor {
     fn deposit_funds(&mut self, deposit: &Vec<Cw20Coin>, state: &Vec<Cw20Coin>) -> Result<Uint128, cosmwasm_std::StdError>;
     fn withdraw_funds(&mut self, shares: &Uint128, state: &Vec<Cw20Coin>) -> Result<Vec<Cw20Coin>, cosmwasm_std::StdError>;
