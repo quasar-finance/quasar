eba571621367da9f811f99c4d6ce765694f78444
akure <ak@quasar.fi>
2022-05-30 07:41:15 +0530

    deposit hooks return fix.

--- x/qbank/keeper/deposit_hooks.go
+++ x/qbank/keeper/deposit_hooks.go
@@ -8,7 +8,10 @@ func (k Keeper) OnDeposit(ctx sdk.Context, vaultID string, coin sdk.Coin) {
 	k.Logger(ctx).Info("OnDeposit", "VaultID", vaultID, "Coin", coin)
 
 	if k.DepositHooks == nil {
+		// This condition comes in the unit testing
 		println("DepositHooks can not be nil")
+		k.Logger(ctx).Info("OnDeposit DepositHooks is nil", "VaultID", vaultID, "Coin", coin)
+		return
 	}
 	k.DepositHooks.OnDeposit(ctx, vaultID, coin)
 }
