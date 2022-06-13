233846efbf924a53109a8de59146c42a0fe9f5ed
LaurensKubat <Laurens.kubat@gmail.com>
2022-06-13 15:01:43 +0200

    add initial curve implementation

--- smart-contracts/packages/quasar-traits/src/traits.rs
+++ smart-contracts/packages/quasar-traits/src/traits.rs
@@ -19,3 +19,21 @@ pub trait ShareDistributor: Debug {
     /// It does not update the state but leaves that up to the smart contract.
     fn withdraw_funds(&mut self, shares: &Uint128, state: &Vec<Cw20Coin>) -> Result<Vec<Cw20Coin>, cosmwasm_std::StdError>;
 }
+
+/// ShareDistributor is the trait describing the logic behind distributing shares within a quasar vault.
+/// A share distributor does not allow for preferential treatment of certain addresses. Preferential
+/// treatment has to be done at contract level.
+/// deposit_funds() and withdraw_funds() should be reversible at the same state.
+pub trait Curve: Debug {
+    /// price returns the current price from the curve. Equal to f(x) on the curve
+    /// The state of the curve should be updated afterwards by the caller
+    fn price() -> Result<Uint128, cosmwasm_std::StdError>;
+    /// deposit() calculates the amount of shares that should be given out in exchange for deposit
+    /// amount of tokens. Equal to F(x)
+    /// The state of the curve should be updated afterwards by the caller
+    fn deposit(&mut self, deposit: &Vec<Cw20Coin>, state: &Vec<Cw20Coin>) -> Result<Uint128, cosmwasm_std::StdError>;
+    /// withdraw() calculates the amount of funds that should be returned in exchange for
+    /// shares amount of shares under the current state in perfect circumstances. equal to F^-1(x)
+    /// The state of the curve should be updated afterwards by the caller
+    fn withdraw(&mut self, shares: &Uint128, state: &Vec<Cw20Coin>) -> Result<Vec<Cw20Coin>, cosmwasm_std::StdError>;
+}
