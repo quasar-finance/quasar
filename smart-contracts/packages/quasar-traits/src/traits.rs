32c6583b6c7d2373e31164b8d2b02f526196f3e7
LaurensKubat <Laurens.kubat@gmail.com>
2022-06-16 10:41:35 +0200

    update cruve trait and add constant curve

--- smart-contracts/packages/quasar-traits/src/traits.rs
+++ smart-contracts/packages/quasar-traits/src/traits.rs
@@ -1,6 +1,6 @@
 use std::fmt::Debug;
 use cw20::{Cw20Coin};
-use cosmwasm_std::{Uint128};
+use cosmwasm_std::{Decimal, Uint128};
 use serde::{Serialize};
 use schemars::JsonSchema;
 use serde::de::DeserializeOwned;
@@ -27,13 +27,13 @@ pub trait ShareDistributor: Debug {
 pub trait Curve: Debug {
     /// price returns the current price from the curve. Equal to f(x) on the curve
     /// The state of the curve should be updated afterwards by the caller
-    fn price() -> Result<Uint128, cosmwasm_std::StdError>;
+    fn price(&self, supply: &Uint128) -> Decimal;
     /// deposit() calculates the amount of shares that should be given out in exchange for deposit
     /// amount of tokens. Equal to F(x)
     /// The state of the curve should be updated afterwards by the caller
-    fn deposit(&mut self, deposit: &Vec<Cw20Coin>, state: &Vec<Cw20Coin>) -> Result<Uint128, cosmwasm_std::StdError>;
+    fn deposit(&self, deposit: &Uint128) -> Uint128;
     /// withdraw() calculates the amount of funds that should be returned in exchange for
     /// shares amount of shares under the current state in perfect circumstances. equal to F^-1(x)
     /// The state of the curve should be updated afterwards by the caller
-    fn withdraw(&mut self, shares: &Uint128, state: &Vec<Cw20Coin>) -> Result<Vec<Cw20Coin>, cosmwasm_std::StdError>;
+    fn withdraw(&self, shares: &Uint128) -> Uint128;
 }
