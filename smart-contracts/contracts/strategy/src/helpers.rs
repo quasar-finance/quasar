19f126931b3f1f08ef4ae90e20a840b5aa073c03
LaurensKubat <Laurens.kubat@gmail.com>
2022-06-27 20:26:11 +0200

    scaffold strategy

---
+++ smart-contracts/contracts/strategy/src/helpers.rs
@@ -0,0 +1,9 @@
+use cosmwasm_std::{Addr, Deps, Order, Uint128};
+use quasar_traits::traits::Curve;
+use crate::state::{VAULT_INFO, VAULT_CURVE};
+
+
+#[cfg(test)]
+mod tests {
+    // TODO write some tests for helpers
+}
