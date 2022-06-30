4186aa7a3234d543431bfde3e9cacce8fe86527f
LaurensKubat <Laurens.kubat@gmail.com>
2022-06-30 12:06:53 +0200

    add initial queue implementation and use in withdraw

--- smart-contracts/contracts/strategy/src/contract.rs
+++ smart-contracts/contracts/strategy/src/contract.rs
@@ -1,40 +1,32 @@
-use std::cmp::min;
-use std::collections::VecDeque;
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::CosmosMsg::Bank;
-use cosmwasm_std::{coins, to_binary, BankMsg, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdError, StdResult, Uint128, Coin, Addr};
+use cosmwasm_std::{
+    coins, to_binary, Addr, BankMsg, Binary, Coin, Deps, DepsMut, Env, MessageInfo, Response,
+    StdError, StdResult, Uint128,
+};
 use cw2::set_contract_version;
-use cw20::{EmbeddedLogo, Logo, LogoInfo, MarketingInfoResponse};
+use std::cmp::min;
+use std::collections::VecDeque;
 
-// TODO decide if we want to use deduct allowance
-use cw20_base::allowances::{
-    deduct_allowance, execute_decrease_allowance, execute_increase_allowance,
-    execute_send_from, execute_transfer_from, query_allowance,
-};
-use cw20_base::contract::{
-    execute_burn, execute_mint, execute_send, execute_transfer, execute_update_marketing,
-    execute_upload_logo, query_balance, query_download_logo, query_marketing_info, query_minter,
-    query_token_info,
-};
-use cw20_base::enumerable::{query_all_accounts, query_all_allowances};
-use cw20_base::state::{MinterData, TokenInfo, LOGO, MARKETING_INFO, TOKEN_INFO};
 use cw_utils::{must_pay, nonpayable};
 use quasar_traits::traits::Curve;
 use quasar_types::curve::{CurveType, DecimalPlaces};
 
-use share_distributor::single_token::SingleToken;
-use crate::ContractError::{PaymentError, Std};
 use crate::error::ContractError;
-use crate::msg::{AssetResponse, ConvertToAssetsResponse, ConvertToSharesResponse, ExecuteMsg, InstantiateMsg, MaxDepositResponse, QueryMsg, TotalAssetResponse, VaultInfoResponse};
-use crate::state::{VaultInfo, VAULT_CURVE, VAULT_INFO, WITHDRAW_QUEUE, WithdrawRequest};
+use crate::error::ContractError::{PaymentError, Std};
+use crate::msg::{
+    InstantiateMsg, QueryMsg, ExecuteMsg
+};
+use crate::queue::{dequeue, enqueue};
+use crate::state::{WITHDRAW_QUEUE, WithdrawRequest};
+use share_distributor::single_token::SingleToken;
 
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:cw-4626";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 const LOGO_SIZE_CAP: usize = 5 * 1024;
-
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
@@ -59,29 +51,92 @@ pub fn execute(
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
     match msg {
-        ExecuteMsg::Deposit { .. } => {todo!()}
-        ExecuteMsg::WithdrawRequest { .. } => {todo!()}
+        // TODO decide if we want to do something with deposit
+        ExecuteMsg::Deposit { .. } => {
+            execute_deposit(deps, env, info)
+        }
+        ExecuteMsg::WithdrawRequest { .. } => {
+            todo!()
+        }
     }
 }
 
-pub fn execute_deposit(deps: DepsMut, env: Env, info: MessageInfo) {
-    let mut queue: VecDeque<WithdrawRequest> = WITHDRAW_QUEUE.load(deps)?;
-    // we want a fifo queue
-    queue.push_back(WithdrawRequest{
-    })
+pub fn execute_deposit(deps: DepsMut, env: Env, info: MessageInfo) -> Result<Response, ContractError> {
+    // do things here with the funds. This is where the actual strategy starts placing funds on a new deposit
+    // in the template version of the contract, all we do is check that funds are present
+    if info.funds.len() == 0 {
+        PaymentError(cw_utils::PaymentError::NoFunds {});
+    }
+    // TODO add some more sensible attributes here
+    Ok(Response::new().add_attribute("deposit", info.sender))
+}
+
+pub fn execute_withdraw_request(
+    mut deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    owner: String,
+) -> Result<Response, ContractError> {
+    enqueue(
+        deps.branch(),
+        WithdrawRequest {
+            denom: info.funds[0].denom.clone(),
+            amount: info.funds[0].amount,
+            owner,
+        },
+    )?;
+    let res = try_withdraw(deps, env)?;
+    todo!()
 
 }
 
-pub fn execute_withdraw_request(deps: DepsMut, env: Env, info: MessageInfo) {
-
+fn try_withdraw(mut deps: DepsMut, env: Env) -> Result<Response, ContractError> {
+    let withdraw = dequeue(deps.branch());
+    if withdraw.is_none() {
+        return Err(ContractError::QueueError("dequeue was none while queue should be some".to_string()));
+    }
+    let w = withdraw.unwrap();
+    // check the free balance of the contract
+    let free_balance = deps
+        .querier
+        .query_balance(env.contract.address, w.denom.clone())?;
+    // if the contract has enough free balance, execute the withdraw
+    if w.amount <= free_balance.amount {
+        // remove the peeked withdraw request
+        return do_withdraw(w);
+    } else {
+        // else we start to unlock funds and return a response
+        // TODO determine whether we need to dequeue the withdraw at this point or a later point
+        unlock_funds(deps, w)
+    }
 }
 
+// do_withdraw sends funds from the contract to the owner of the funds according to the withdraw request
+fn do_withdraw(withdraw: WithdrawRequest) -> Result<Response, ContractError> {
+
+    let msg = BankMsg::Send {
+        to_address: withdraw.owner.clone(),
+        amount: coins(withdraw.amount.u128(), withdraw.denom.clone()),
+    };
+    Ok(Response::new()
+        .add_message(msg)
+        .add_attribute("withdraw", "executed")
+        .add_attribute("amount", withdraw.amount)
+        .add_attribute("denom", withdraw.denom)
+        .add_attribute("owner", withdraw.owner))
+}
+
+fn unlock_funds(deps: DepsMut, withdraw: WithdrawRequest) -> Result<Response, ContractError> {
+    // TODO this is where funds are locked or not present within the strategy contract. The withdraw happens async here
+    // the strategy needs to know where the funds are located, unlock the funds there(or do so)
+    todo!()
+}
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
-        .. => todo!()
     }
+    todo!()
 }
 
 mod tests {
@@ -104,14 +159,17 @@ mod tests {
         InstantiateMsg {}
     }
 
-    fn get_balance<U: Into<String>>(deps: Deps, addr: U) -> Uint128 {
-        query_balance(deps, addr.into()).unwrap().balance
-    }
 
     fn setup_test(
         deps: DepsMut,
         supply_decimals: u8,
         reserve_decimals: u8,
         reserve_supply: Uint128,
-    ) {}
+    ) {
+    }
+
+    #[test]
+    fn deposit_works() {
+
+    }
 }
--- smart-contracts/contracts/strategy/src/state.rs
+++ smart-contracts/contracts/strategy/src/state.rs
@@ -1,6 +1,6 @@
-use std::collections::VecDeque;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use std::collections::VecDeque;
 use std::fmt::Debug;
 use uuid::Uuid;
 
@@ -12,12 +12,13 @@ use quasar_types::curve::{CurveType, DecimalPlaces};
 use serde::de::DeserializeOwned;
 use share_distributor::single_token::SingleToken;
 
-
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug, Default)]
+#[serde(rename_all = "snake_case")]
 pub struct WithdrawRequest {
-    denom: String,
-    amount: Uint128,
-    // an id so the vault knows which withdrawal is being fulfilled
-    id: Uuid,
+    pub denom: String,
+    pub amount: Uint128,
+    pub owner: String,
 }
 
-pub const WITHDRAW_QUEUE: Item<VecDeque<WithdrawRequest>> = Item::new("withdraw_queue");
+// TODO is u128 too much/ insufficient?, this might cause errors on overlapping keys, could also be handled as a full queue error
+pub(crate) const WITHDRAW_QUEUE: Map<u128, WithdrawRequest> = Map::new("withdraw_queue");
