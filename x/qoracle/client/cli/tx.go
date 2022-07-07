407b819739232d7c51f71a49e1fa77b07e96f7ec
Ehsan-saradar <ehsan.saradar@gmail.com>
2022-07-08 00:50:35 +0430

    Fix bugs

--- x/qoracle/client/cli/tx.go
+++ x/qoracle/client/cli/tx.go
@@ -35,6 +35,7 @@ func GetTxCmd() *cobra.Command {
 	cmd.AddCommand(CmdCreatePoolInfo())
 	cmd.AddCommand(CmdUpdatePoolInfo())
 	cmd.AddCommand(CmdDeletePoolInfo())
+	cmd.AddCommand(CmdStablePrice())
 	// this line is used by starport scaffolding # 1
 
 	return cmd
