a9a8d3464e263fdf03bf42201d44a833bfe5bc14
Ehsan-saradar <ehsan.saradar@gmail.com>
2022-07-15 21:08:21 +0430

    Implement osmosis icq packet handling

---
+++ x/qoracle/types/osmosis.go
@@ -0,0 +1,13 @@
+package types
+
+const (
+	OsmosisQueryEpochsInfoPath          = "/osmosis.epochs.v1beta1.Query/EpochInfos"
+	OsmosisQueryPoolPath                = "/osmosis.gamm.v1beta1.Query/Pool"
+	OsmosisQueryLockableDurationsPath   = "/osmosis.poolincentives.v1beta1.Query/LockableDurations"
+	OsmosisQueryMintParamsPath          = "/osmosis.mint.v1beta1.Query/Params"
+	OsmosisQueryMintEpochProvisionsPath = "/osmosis.mint.v1beta1.Query/EpochProvisions"
+	OsmosisQueryIncentivizedPoolsPath   = "/osmosis.poolincentives.v1beta1.Query/IncentivizedPools"
+	OsmosisQueryPoolGaugeIdsPath        = "/osmosis.poolincentives.v1beta1.Query/GaugeIds"
+	OsmosisQueryDistrInfoPath           = "/osmosis.poolincentives.v1beta1.Query/DistrInfo"
+	OsmosisQuerySpotPricePath           = "/osmosis.gamm.v1beta1.Query/SpotPrice"
+)
