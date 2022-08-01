530ad1bf20ec95e441358e1597c57b3b422cd55e
LaurensKubat <Laurens.kubat@gmail.com>
2022-08-01 11:50:23 +0200

    wip

---
+++ smart-contracts/contracts/remote_strategy/src/helpers.rs
@@ -0,0 +1,8 @@
+use cosmwasm_std::{Addr, Deps, Order, Uint128};
+
+
+
+#[cfg(test)]
+mod tests {
+    // TODO write some tests for helpers
+}
---
+++ smart-contracts/packages/osmosis-helpers/src/osmosis.rs
@@ -0,0 +1,5 @@
+use osmo_bindings::{OsmosisMsg, OsmosisQuery};
+
+pub fn join_pool(pool_id: u64) {
+
+}
