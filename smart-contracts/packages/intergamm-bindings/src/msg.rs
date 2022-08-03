1ac7dfe252e45bf542b97958381988e46b97a962
LaurensKubat <Laurens.kubat@gmail.com>
2022-08-03 19:35:55 +0200

    add junk

---
+++ smart-contracts/packages/intergamm-bindings/src/msg.rs
@@ -0,0 +1,37 @@
+use cosmwasm_std::{Coin, CosmosMsg, CustomMsg, Timestamp, Uint64};
+use schemars::JsonSchema;
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+#[serde(rename_all = "snake_case")]
+/// A number of Custom messages that can call into the intergamm bindings
+pub enum IntergammMsg {
+    SendToken {
+        creator: String,
+        destination_local_zone_id: String,
+        sender: String,
+        receiver: String,
+        coin: Coin
+    },
+    TransmitIbcJoinPool {
+        creator: String,
+        connection_id: String,
+        timeout_timestamp: Timestamp,
+        pool_id: Uint64,
+    }
+
+
+}
+
+impl IntergammMsg {
+
+}
+
+impl From<IntergammMsg> for CosmosMsg<IntergammMsg> {
+    fn from(msg: IntergammMsg) -> CosmosMsg<IntergammMsg> {
+        CosmosMsg::Custom(msg)
+    }
+}
+
+impl CustomMsg for IntergammMsg {}
