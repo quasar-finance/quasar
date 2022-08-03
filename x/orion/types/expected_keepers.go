88eed3aa475e090a6cd0404a1c5f56112e9e45ac
LaurensKubat <Laurens.kubat@gmail.com>
2022-08-03 20:39:21 +0200

    wip pair programming 2

--- x/orion/types/expected_keepers.go
+++ x/orion/types/expected_keepers.go
@@ -74,7 +74,7 @@ type IntergammKeeper interface {
 	GetConnectionId(ctx sdk.Context, inChainID string) (string, bool)
 
 	SendToken(ctx sdk.Context,
-		destinationLocalZoneId string,
+		destination_local_zone_id string,
 		sender sdk.AccAddress,
 		receiver string,
 		coin sdk.Coin) (uint64, error)
