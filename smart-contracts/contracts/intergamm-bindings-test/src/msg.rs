1640cdfbf6fdb7cbc1c66bf9ec1b37a16238e3f3
LaurensKubat <Laurens.kubat@gmail.com>
2022-08-03 20:31:54 +0200

    wip pair programming

--- smart-contracts/contracts/intergamm-bindings-test/src/msg.rs
+++ smart-contracts/contracts/intergamm-bindings-test/src/msg.rs
@@ -3,25 +3,18 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
-    pub count: i32,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    Increment {},
-    Reset { count: i32 },
+    SendToken{},
+    JoinPool{}
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
-    GetCount {},
+
 }
 
-// We define a custom struct for each query response
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct GetCountResponse {
-    pub count: i32,
-}
