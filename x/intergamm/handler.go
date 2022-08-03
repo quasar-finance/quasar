18538ea1e5fe691dfea4f9e296735208034d8ddf
njerschow <nj18@nyu.edu>
2022-08-03 16:47:56 -0400

    try-fix

--- x/intergamm/handler.go
+++ x/intergamm/handler.go
@@ -35,6 +35,9 @@ func NewHandler(k *keeper.Keeper) sdk.Handler {
 		case *types.MsgTestScenario:
 			res, err := msgServer.TestScenario(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgSendToken:
+			res, err := msgServer.SendToken((sdk.WrapSDKContext(ctx)), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 			// this line is used by starport scaffolding # 1
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
