aa39f5f948cd10d6233223665c48ef87010800f1
Ehsan-saradar <ehsan.saradar@gmail.com>
2022-08-10 02:38:53 +0430

    Fix not sending osmosis icq packets on epoch

--- x/qoracle/types/query.pb.gw.go
+++ x/qoracle/types/query.pb.gw.go
@@ -455,9 +455,20 @@ func local_request_Query_OsmosisIcentivizedPools_0(ctx context.Context, marshale
 
 }
 
+var (
+	filter_Query_OsmosisPools_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
 func request_Query_OsmosisPools_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq QueryOsmosisPoolsRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_OsmosisPools_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := client.OsmosisPools(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
@@ -467,6 +478,13 @@ func request_Query_OsmosisPools_0(ctx context.Context, marshaler runtime.Marshal
 func local_request_Query_OsmosisPools_0(ctx context.Context, marshaler runtime.Marshaler, server QueryServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq QueryOsmosisPoolsRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_OsmosisPools_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := server.OsmosisPools(ctx, &protoReq)
 	return msg, metadata, err
@@ -1105,9 +1123,9 @@ var (
 
 	pattern_Query_OsmosisChainParams_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"abag", "quasarnode", "qoracle", "osmosis", "chain_params"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_Query_OsmosisIcentivizedPools_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"abag", "quasarnode", "qoracle", "osmosis_icentivized_pools"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_OsmosisIcentivizedPools_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"abag", "quasarnode", "qoracle", "osmosis", "icentivized_pools"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_Query_OsmosisPools_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"abag", "quasarnode", "qoracle", "osmosis_pools"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_OsmosisPools_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"abag", "quasarnode", "qoracle", "osmosis", "pools"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
