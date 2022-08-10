6dbbe5ae9baeff09b7a4033abac5e433ecfdb3b5
TheRagnarLodbrok <ehsan.saradar@gmail.com>
2022-08-10 12:33:40 +0430

    Fix Denom mapping

--- x/qoracle/keeper/stable_price.go
+++ x/qoracle/keeper/stable_price.go
@@ -9,44 +9,47 @@ import (
 
 // SetStablePrice set the stable price for the symbol
 func (k Keeper) SetStablePrice(ctx sdk.Context, symbol string, price sdk.Dec) {
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyStablePricesPrefix)
+  denomMapping := k.GetDenomPriceMappings(ctx)
+  denomMapping = append(denomMapping, types.DenomPriceMapping{
+    Denom:       symbol,
+    OracleDenom: symbol,
+    Multiplier:  sdk.NewDec(1),
+  })
+  k.SetDenomPriceMappings(ctx, denomMapping)
 
-	key := []byte(symbol)
-	b, err := price.Marshal()
-	if err != nil {
-		panic(err)
-	}
-	store.Set(key, b)
+  op := k.GetOraclePrices(ctx)
+  op.Prices = op.Prices.Add(sdk.NewDecCoinFromDec(symbol, price))
+  k.setOraclePrices(ctx, op)
 }
 
 // GetStablePrice get the stable denom for the symbol
 func (k Keeper) GetStablePrice(ctx sdk.Context, symbol string) (price sdk.Dec, found bool) {
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyStablePricesPrefix)
-	key := []byte(symbol)
-	b := store.Get(key)
-	if b == nil {
-		return price, false
-	}
-
-	err := (&price).Unmarshal(b)
-	if err != nil {
-		return price, false
-	}
-	return price, true
+  denomMapping := k.GetDenomPriceMappings(ctx)
+  for _, d := range denomMapping {
+    if d.OracleDenom == symbol {
+      op := k.GetOraclePrices(ctx)
+      amount := op.Prices.AmountOf(d.OracleDenom)
+      if amount.IsZero() {
+        return price, false
+      }
+      return amount.Mul(d.Multiplier), true
+    }
+  }
+  return price, false
 }
 
 // GetRelativeStablePrice calculates how many denomOut is equivalent to one denomIn.
 func (k Keeper) GetRelativeStablePrice(ctx sdk.Context, denomIn, denomOut string) (sdk.Dec, error) {
-	priceIn, found := k.GetStablePrice(ctx, denomIn)
-	if !found {
-		return sdk.Dec{}, sdkerrors.Wrapf(types.ErrStablePriceNotFound, "stable price not found for denom %s", denomIn)
-	}
-	priceOut, found := k.GetStablePrice(ctx, denomOut)
-	if !found {
-		return sdk.Dec{}, sdkerrors.Wrapf(types.ErrStablePriceNotFound, "stable price not found for denom %s", denomOut)
-	}
-	if priceOut.IsZero() {
-		return sdk.Dec{}, sdkerrors.Wrapf(types.ErrZeroStablePrice, "zero stable price for denom %s", denomOut)
-	}
-	return priceIn.Quo(priceOut), nil
+  priceIn, found := k.GetStablePrice(ctx, denomIn)
+  if !found {
+    return sdk.Dec{}, sdkerrors.Wrapf(types.ErrStablePriceNotFound, "stable price not found for denom %s", denomIn)
+  }
+  priceOut, found := k.GetStablePrice(ctx, denomOut)
+  if !found {
+    return sdk.Dec{}, sdkerrors.Wrapf(types.ErrStablePriceNotFound, "stable price not found for denom %s", denomOut)
+  }
+  if priceOut.IsZero() {
+    return sdk.Dec{}, sdkerrors.Wrapf(types.ErrZeroStablePrice, "zero stable price for denom %s", denomOut)
+  }
+  return priceIn.Quo(priceOut), nil
 }
