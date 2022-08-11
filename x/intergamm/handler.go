356eeb64bcf30ba8230ebfd82416577555e574e9
njerschow <nj18@nyu.edu>
2022-08-11 14:05:16 -0400

    add handler routes

--- x/intergamm/handler.go
+++ x/intergamm/handler.go
@@ -36,7 +36,25 @@ func NewHandler(k *keeper.Keeper) sdk.Handler {
 			res, err := msgServer.TestScenario(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgSendToken:
-			res, err := msgServer.SendToken((sdk.WrapSDKContext(ctx)), msg)
+			res, err := msgServer.SendToken(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgTransmitIbcJoinPool:
+			res, err := msgServer.TransmitIbcJoinPool(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgTransmitIbcExitPool:
+			res, err := msgServer.TransmitIbcExitPool(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgTransmitIbcLockTokens:
+			res, err := msgServer.TransmitIbcLockTokens(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgTransmitIbcBeginUnlocking:
+			res, err := msgServer.TransmitIbcBeginUnlocking(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgTransmitIbcJoinSwapExternAmountIn:
+			res, err := msgServer.TransmitIbcJoinSwapExternAmountIn(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgTransmitIbcExitSwapExternAmountOut:
+			res, err := msgServer.TransmitIbcExitSwapExternAmountOut(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
 			// this line is used by starport scaffolding # 1
 		case *types.MsgSendToken:
