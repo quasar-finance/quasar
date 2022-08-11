0443f8d126553a3b3be967f444a2ee1943ef3424
njerschow <nj18@nyu.edu>
2022-08-11 15:06:51 -0400

    add rest of intergamm wasmbindings

--- wasmbinding/bindings/msg.go
+++ wasmbinding/bindings/msg.go
@@ -8,6 +8,24 @@ type QuasarMsg struct {
 
 	/// Contracts can send tokens
 	SendToken *SendToken `json:"send_token,omitempty"`
+
+	/// Contracts can transmit JoinPool Messages over IBC
+	OsmosisJoinPool *OsmosisJoinPool `json:"join_pool,omitempty"`
+
+	/// Contracts can transmit ExitPool Messages over IBC
+	OsmosisExitPool *OsmosisExitPool `json:"exit_pool,omitempty"`
+
+	/// Contracts can transmit LockTokens Messages over IBC
+	OsmosisLockTokens *OsmosisLockTokens `json:"lock_tokens,omitempty"`
+
+	/// Contracts can start the unbonding process over IBC
+	OsmosisBeginUnlocking *OsmosisBeginUnlocking `json:"begin_unlocking,omitempty"`
+
+	// Contracts can transmit JoinSwapExternAmountIn Messages over IBC
+	OsmosisJoinSwapExternAmountIn *OsmosisJoinSwapExternAmountIn `json:"join_swap_extern_amount_in,omitempty"`
+
+	// Contracts can transmit ExitSwapExternAmountOut Messages over IBC
+	OsmosisExitSwapExternAmountOut *OsmosisExitSwapExternAmountOut `json:"exit_swap_extern_amount_out,omitempty"`
 }
 
 type TestScenario struct {
@@ -21,4 +39,56 @@ type SendToken struct {
 	Sender                 string   `json:"sender"`
 	Receiver               string   `json:"receiver"`
 	Coin                   sdk.Coin `json:"coin"`
+}
+
+type OsmosisJoinPool struct {
+	Creator          string     `json:"creator"`
+	ConnectionId     string     `json:"connection_id"`
+	TimeoutTimestamp uint64     `json:"timeout_timestamp"`
+	PoolId           uint64     `json:"pool_id"`
+	ShareOutAmount   int64      `json:"share_out_amount"`
+	TokenInMaxs      []sdk.Coin `json:"token_in_maxs"`
+}
+
+type OsmosisExitPool struct {
+	Creator          string     `json:"creator"`
+	ConnectionId     string     `json:"connection_id"`
+	TimeoutTimestamp uint64     `json:"timeout_timestamp"`
+	PoolId           uint64     `json:"pool_id"`
+	ShareInAmount    int64      `json:"share_in_amount"`
+	TokenOutMins     []sdk.Coin `json:"token_out_mins"`
+}
+
+type OsmosisLockTokens struct {
+	Creator          string     `json:"creator"`
+	ConnectionId     string     `json:"connection_id"`
+	TimeoutTimestamp uint64     `json:"timeout_timestamp"`
+	Duration         uint64     `json:"duration"`
+	Coins            []sdk.Coin `json:"coins"`
+}
+
+type OsmosisBeginUnlocking struct {
+	Creator          string     `json:"creator"`
+	ConnectionId     string     `json:"connection_id"`
+	TimeoutTimestamp uint64     `json:"timeout_timestamp"`
+	Id               uint64     `json:"id"`
+	Coins            []sdk.Coin `json:"coins"`
+}
+
+type OsmosisJoinSwapExternAmountIn struct {
+	Creator           string   `json:"creator"`
+	ConnectionId      string   `json:"connection_id"`
+	TimeoutTimestamp  uint64   `json:"timeout_timestamp"`
+	PoolId            uint64   `json:"pool_id"`
+	ShareOutMinAmount int64    `json:"share_out_min_amount"`
+	TokenIn           sdk.Coin `json:"token_in"`
+}
+
+type OsmosisExitSwapExternAmountOut struct {
+	Creator          string   `json:"creator"`
+	ConnectionId     string   `json:"connection_id"`
+	TimeoutTimestamp uint64   `json:"timeout_timestamp"`
+	PoolId           uint64   `json:"pool_id"`
+	ShareInAmount    int64    `json:"share_in_amount"`
+	TokenOutMins     sdk.Coin `json:"token_out_mins"`
 }
