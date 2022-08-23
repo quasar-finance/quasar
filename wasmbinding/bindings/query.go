f44fb5c43bffd5a6dfc6dc4cffd4e3d64dc8da87
njerschow <nj18@nyu.edu>
2022-08-23 15:42:26 -0400

    [WIP] qOracle wasmbinding

--- wasmbinding/bindings/query.go
+++ wasmbinding/bindings/query.go
@@ -1,6 +1,30 @@
 package bindings
 
+import (
+	qoracletypes "github.com/quasarlabs/quasarnode/x/qoracle/types"
+)
+
 // OsmosisQuery contains osmosis custom queries.
 // See https://github.com/osmosis-labs/osmosis-bindings/blob/main/packages/bindings/src/query.rs
 type QuasarQuery struct {
+	// Query Params RPC Method
+	QueryParamsRequest *qoracletypes.QueryParamsRequest `json:"query_params_request,omitempty"`
+
+	// Query our position within a specific pool
+	QueryGetPoolPositionRequest *qoracletypes.QueryGetPoolPositionRequest `json:"query_get_pool_position_request,omitempty"`
+
+	// Query a list of pool positions
+	QueryPoolPositionsRequest *qoracletypes.QueryAllPoolPositionRequest `json:"query_pool_positions_request,omitempty"`
+
+	// Query the ranking of pools
+	QueryPoolRankingRequest *qoracletypes.QueryGetPoolRankingRequest `json:"query_pool_ranking_request,omitempty"`
+
+	// Query pool info
+	QueryPoolInfoRequest *qoracletypes.QueryAllPoolInfoRequest `json:"query_pool_info_request,omitempty"`
+
+	// Query all pool info
+	QueryAllPoolInfoRequest *qoracletypes.QueryAllPoolInfoRequest `json:"query_all_pool_info_request,omitempty"`
+
+	// Query oracle prices
+	QueryOraclePricesRequest *qoracletypes.QueryOraclePricesRequest `json:"query_oracle_prices_request,omitempty"`
 }
--- wasmbinding/queries.go
+++ wasmbinding/queries.go
@@ -1,11 +1,17 @@
 package wasmbinding
 
 import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	intergammkeeper "github.com/quasarlabs/quasarnode/x/intergamm/keeper"
+	qoraclekeeper "github.com/quasarlabs/quasarnode/x/qoracle/keeper"
+	"github.com/quasarlabs/quasarnode/x/qoracle/types"
 )
 
 type QueryPlugin struct {
 	intergammKeeper *intergammkeeper.Keeper
+	qoracleKeeper   *qoraclekeeper.Keeper
 }
 
 // NewQueryPlugin returns a reference to a new QueryPlugin.
@@ -13,4 +19,13 @@ func NewQueryPlugin(gk *intergammkeeper.Keeper) *QueryPlugin {
 	return &QueryPlugin{
 		intergammKeeper: gk,
 	}
+}
+
+func (qp QueryPlugin) GetPoolInfo(ctx sdk.Context, poolID string) (types.PoolInfo, error) {
+	pool, found := qp.qoracleKeeper.GetPoolInfo(ctx, poolID)
+	if !found {
+		return nil, fmt.Errorf("failed to find pool for poolID: %s", poolID)
+	}
+
+	return pool, nil
 }
