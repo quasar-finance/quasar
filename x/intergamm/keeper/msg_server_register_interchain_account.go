e123ba72e277a88aa6ad2eafcf4003a9e143b237
LaurensKubat <Laurens.kubat@gmail.com>
2022-08-25 17:49:44 +0200

    WIP binding registerInterchainAccount

--- x/intergamm/keeper/msg_server_register_interchain_account.go
+++ x/intergamm/keeper/msg_server_register_interchain_account.go
@@ -4,7 +4,10 @@ import (
 	"context"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	icatypes "github.com/cosmos/ibc-go/v3/modules/apps/27-interchain-accounts/types"
 	"github.com/quasarlabs/quasarnode/x/intergamm/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (ms msgServer)RegisterInterchainAccount(goCtx context.Context, register *types.MsgRegisterInterchainAccount) (*types.MsgRegisterInterchainAccountResponse, error) {
@@ -15,9 +18,17 @@ func (ms msgServer)RegisterInterchainAccount(goCtx context.Context, register *ty
 		return nil, err
 	}
 
-	err = ms.k.RegisterInterchainAccount(ctx, register.ConnectionId, creator.String())
+	portID, err := icatypes.NewControllerPortID(creator.String())
 	if err != nil {
-		return nil, err
+		return nil, status.Errorf(codes.InvalidArgument, "could not generate port for address: %s", err)
+	}
+
+	_, found := ms.k.icaControllerKeeper.GetOpenActiveChannel(ctx, register.ConnectionId, portID)
+	if !found {
+		err = ms.k.RegisterInterchainAccount(ctx, register.ConnectionId, creator.String())
+		if err != nil {
+			return nil, err
+		}
 	}
 	return &types.MsgRegisterInterchainAccountResponse{}, nil
 }
