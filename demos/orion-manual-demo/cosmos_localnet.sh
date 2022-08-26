fe48afd33c5b73e1fe405ed1250e42fb5e634db1
njerschow <nj18@nyu.edu>
2022-08-26 10:12:28 -0400

    update build scripts for mac

--- demos/orion-manual-demo/cosmos_localnet.sh
+++ demos/orion-manual-demo/cosmos_localnet.sh
@@ -20,13 +20,13 @@ rm -rf $HOME_COSMOSHUB
 
 # Bootstrap
 $BINARY init $CHAIN_ID --chain-id $CHAIN_ID --home $HOME_COSMOSHUB
-echo $ALICE  | $BINARY keys add alice --keyring-backend test --recover
-echo $BOB    | $BINARY keys add bob   --keyring-backend test --recover
+echo $ALICE | $BINARY keys add alice --keyring-backend test --recover
+echo $BOB | $BINARY keys add bob --keyring-backend test --recover
 echo $USER_1 | $BINARY keys add user1 --keyring-backend test --recover
 echo $USER_2 | $BINARY keys add user2 --keyring-backend test --recover
 echo $RELAYER_ACC | $BINARY keys add relayer_acc --keyring-backend test --recover
-$BINARY add-genesis-account $($BINARY keys show alice --keyring-backend test -a) $ALICE_GENESIS_COINS  --home $HOME_COSMOSHUB
-$BINARY add-genesis-account $($BINARY keys show bob   --keyring-backend test -a) $BOB_GENESIS_COINS    --home $HOME_COSMOSHUB
+$BINARY add-genesis-account $($BINARY keys show alice --keyring-backend test -a) $ALICE_GENESIS_COINS --home $HOME_COSMOSHUB
+$BINARY add-genesis-account $($BINARY keys show bob --keyring-backend test -a) $BOB_GENESIS_COINS --home $HOME_COSMOSHUB
 $BINARY add-genesis-account $($BINARY keys show user1 --keyring-backend test -a) $USER_1_GENESIS_COINS --home $HOME_COSMOSHUB
 $BINARY add-genesis-account $($BINARY keys show user2 --keyring-backend test -a) $USER_2_GENESIS_COINS --home $HOME_COSMOSHUB
 $BINARY add-genesis-account $($BINARY keys show relayer_acc --keyring-backend test -a) $RELAYER_ACC_GENESIS_COINS --home $HOME_COSMOSHUB
@@ -36,7 +36,7 @@ $BINARY collect-gentxs
 
 # Check platform
 platform='unknown'
-unamestr=`uname`
+unamestr=$(uname)
 if [ "$unamestr" = 'Linux' ]; then
    	platform='linux'
 elif [ "$unamestr" = 'Darwin' ]; then
@@ -48,7 +48,7 @@ if [ $platform = 'linux' ]; then
 	sed -i 's/swagger = false/swagger = true/g' $HOME_COSMOSHUB/config/app.toml
 	sed -i 's/minimum-gas-prices = ""/minimum-gas-prices = "0uatom"/g' $HOME_COSMOSHUB/config/app.toml
 	sed -i 's+laddr = "tcp://127.0.0.1:26657"+laddr = "tcp://127.0.0.1:26669"+g' $HOME_COSMOSHUB/config/config.toml
-	sed -i 's+node = "tcp://localhost:26657"+node = "tcp://localhost:26669"+g' $HOME_COSMOSHUB/config/client.toml	
+	sed -i 's+node = "tcp://localhost:26657"+node = "tcp://localhost:26669"+g' $HOME_COSMOSHUB/config/client.toml
 	sed -i 's+laddr = "tcp://0.0.0.0:26656"+laddr = "tcp://0.0.0.0:26663"+g' $HOME_COSMOSHUB/config/config.toml
 	sed -i 's+pprof_laddr = "localhost:6060"+pprof_laddr = "localhost:6063"+g' $HOME_COSMOSHUB/config/config.toml
 	sed -i 's+address = "0.0.0.0:9090"+address = "0.0.0.0:9097"+g' $HOME_COSMOSHUB/config/app.toml
@@ -76,14 +76,14 @@ fi
 
 cp $HOME_COSMOSHUB/config/genesis.json $HOME_COSMOSHUB/config/genesis_original.json
 cat $HOME_COSMOSHUB/config/genesis_original.json |
-  jq '.app_state.crisis.constant_fee.denom="uatom"' |
-  jq '.app_state.staking.params.bond_denom="uatom"' |
-  jq '.app_state.mint.params.mint_denom="uatom"' |
-  jq '.app_state.liquidity.params.pool_creation_fee=[{denom:"uatom",amount:"1"}]' |
-  jq '.app_state.gov.deposit_params.min_deposit=[{denom:"uatom",amount:"1"}]' |
-  jq '.app_state.gov.voting_params.voting_period="30s"' |
-  jq '.app_state.gov.tally_params={quorum:"0.000000000000000001",threshold:"0.5",veto_threshold:"0.334"}' \
-  >  $HOME_COSMOSHUB/config/genesis.json
+	jq '.app_state.crisis.constant_fee.denom="uatom"' |
+	jq '.app_state.staking.params.bond_denom="uatom"' |
+	jq '.app_state.mint.params.mint_denom="uatom"' |
+	jq '.app_state.liquidity.params.pool_creation_fee=[{denom:"uatom",amount:"1"}]' |
+	jq '.app_state.gov.deposit_params.min_deposit=[{denom:"uatom",amount:"1"}]' |
+	jq '.app_state.gov.voting_params.voting_period="30s"' |
+	jq '.app_state.gov.tally_params={quorum:"0.000000000000000001",threshold:"0.5",veto_threshold:"0.334"}' \
+		>$HOME_COSMOSHUB/config/genesis.json
 
 # Start
 $BINARY start
