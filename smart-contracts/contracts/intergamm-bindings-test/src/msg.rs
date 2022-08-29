25ef0c5f7977d1073b5ad8af040c677f405897e5
LaurensKubat <Laurens.kubat@gmail.com>
2022-08-29 22:01:48 +0200

    add instructions and WIP contract

--- smart-contracts/contracts/intergamm-bindings-test/src/msg.rs
+++ smart-contracts/contracts/intergamm-bindings-test/src/msg.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::{Coin, Uint64};
+use cosmwasm_std::{Coin, IbcTimeout, Uint64};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
@@ -10,6 +10,17 @@ pub struct InstantiateMsg {}
 pub enum ExecuteMsg {
     SendToken {
         destination_local_zone_id: String,
+    },
+    SendTokenIbc {
+        /// exisiting channel to send the tokens over
+        channel_id: String,
+        /// address on the remote chain to receive these tokens
+        to_address: String,
+        /// packet data only supports one coin
+        /// https://github.com/cosmos/cosmos-sdk/blob/v0.40.0/proto/ibc/applications/transfer/v1/transfer.proto#L11-L20
+        amount: Coin,
+        /// when packet times out, measured on remote chain
+        timeout: IbcTimeout,
     },
     Deposit {},
     RegisterInterchainAccount {
