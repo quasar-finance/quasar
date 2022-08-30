1a6e23f2d0448d90ed1d12efefdb22cad028dc19
njerschow <nj18@nyu.edu>
2022-08-30 14:44:35 -0400

    WIP rust quasar-bindings package

---
+++ smart-contracts/packages/quasar-bindings/src/lib.rs
@@ -0,0 +1,13 @@
+pub mod msg;
+pub mod query;
+mod querier;
+mod types;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
