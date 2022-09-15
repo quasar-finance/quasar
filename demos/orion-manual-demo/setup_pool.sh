1cd07ccabdb38177b8f4402e30662bdd5431141c
LaurensKubat <Laurens.kubat@gmail.com>
2022-09-15 17:14:08 +0200

    more cleanup

--- demos/orion-manual-demo/setup_pool.sh
+++ demos/orion-manual-demo/setup_pool.sh
@@ -6,7 +6,4 @@ POOLID=$(osmosisd query gamm pools --output json --node tcp://localhost:26679 | 
 POOLADDR=$(osmosisd query gamm pools --output json --node tcp://localhost:26679 | jq .pools[0].address)
 echo "got poolID $POOLID with address $POOLADDR" 
 
-JOINPOOLMSG='{"join_single_pool":{"connection_id":"connection-2","pool_id":"1", "share_out_min_amount":1,"token_in":{"denom":"uosmo", "amount":"12345"}}}'
-echo "sending join pool from quasar"
-quasarnoded tx wasm execute quasar17p9rzwnnfxcjp32un9ug7yhhzgtkhvl9jfksztgw5uh69wac2pgsstgms7 '{"join_single_pool":{"connection_id":"connection-2","pool_id":"1", "share_out_min_amount":1,"token_in":{"denom":"uosmo", "amount":"12345"}}}' --from alice --gas auto --node http://0.0.0.0:26659 --chain-id quasar
 
