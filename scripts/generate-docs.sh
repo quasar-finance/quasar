2332e3a7b1062aece48729184d5d4c1f1cb6b2ff
Ehsan-saradar <ehsan.saradar@gmail.com>
2022-09-18 18:24:48 +0430

    Clean proto files and doc generating scripts

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -3,14 +3,17 @@
 set -Cue -o pipefail
 
 project_dir="$(cd "$(dirname "${0}")/.." ; pwd)" # Absolute path to project dir
-build_dir="${BUILD_DIR:-"${project_dir}/build"}"
-docs_dir="${build_dir}/docs"
-out_dir="${docs_dir}/grpc-swagger-doc"
-tmp_dir="${docs_dir}/proto"
+docs_dir="${project_dir}/docs"
+tmp_dir="${project_dir}/tmp-swagger-gen"
 
 # Get the path of the cosmos-sdk repo from go/pkg/mod
 locate_cosmos_sdk_dir() {
   go list -f "{{ .Dir }}" -m github.com/cosmos/cosmos-sdk
+}
+
+# Get the path of the ibc-go repo from go/pkg/mod
+locate_ibc_go_dir() {
+  go list -f "{{ .Dir }}" -m github.com/cosmos/ibc-go/v3
 }
 
 # Collect all proto dirs
@@ -18,19 +21,21 @@ collect_proto_dirs() {
   find "$@" -path -prune -o -name "*.proto" -print0 | xargs -0 -n1 dirname | sort | uniq
 }
 
-rm -rf "$out_dir"
-mkdir -p "$docs_dir" "$tmp_dir" "$out_dir"
+mkdir -p "$tmp_dir"
 trap "rm -rf ${tmp_dir}" 0
 
 cosmos_sdk_dir="$(locate_cosmos_sdk_dir)"
+ibc_go_dir="$(locate_ibc_go_dir)"
 
 while read -r proto_dir <&3 ; do
   query_file="$(find "${proto_dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))"
   if [[ ! -z "$query_file" ]]; then
+    echo "$query_file"
     protoc \
       -I "${project_dir}/proto" \
       -I "${cosmos_sdk_dir}/third_party/proto" \
       -I "${cosmos_sdk_dir}/proto" \
+      -I "${ibc_go_dir}/proto" \
       "$query_file" \
       --swagger_out "$tmp_dir" \
       --swagger_opt logtostderr=true \
@@ -40,8 +45,9 @@ while read -r proto_dir <&3 ; do
 done 3< <(collect_proto_dirs "${project_dir}/proto" "${cosmos_sdk_dir}/proto")
 
 (
-  cd "${project_dir}/docs/client"
+  cd "$docs_dir"
 
-  npm install
-  OUT_FILE="${out_dir}/swagger.yaml" npm run build
+  yarn install
+  yarn combine
+  yarn convert
 )
