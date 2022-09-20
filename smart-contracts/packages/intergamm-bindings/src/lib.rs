3d1956261898ca2088add71461e3ccb803b08bb1
LaurensKubat <Laurens.kubat@gmail.com>
2022-09-20 16:18:23 +0200

    more cleanup

--- smart-contracts/packages/intergamm-bindings/src/lib.rs
+++ smart-contracts/packages/intergamm-bindings/src/lib.rs
@@ -4,12 +4,3 @@ mod querier;
 mod query;
 mod state;
 mod types;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
