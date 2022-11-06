240a4baf75024d79f8abace988f81e15c6dc018b
Ehsan-saradar <ehsan.saradar@gmail.com>
2022-11-06 23:01:59 +0330

    Initialize e2e test package

---
+++ tests/e2e/config/genesis.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/icza/dyno"
+	"github.com/strangelove-ventures/ibctest/v5/ibc"
+)
+
+// genesisModifiers takes genesis as an unmarshaled "any" value and, modifies it and returns the "any" back.
+type genesisModifiers func(gen any) (any, error)
+
+// modifyGenesis is a helper function that chains multiple genesisModifiers funcs in order to make
+// chain configuration easier and a lot cleaner.
+func modifyGenesis(mods ...genesisModifiers) func(cc ibc.ChainConfig, genbz []byte) ([]byte, error) {
+	return func(cc ibc.ChainConfig, genbz []byte) ([]byte, error) {
+		var gen any
+		err := json.Unmarshal(genbz, &gen)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, mod := range mods {
+			gen, err = mod(gen)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		return json.Marshal(gen)
+	}
+}
+
+// modifyGenesisSetVotingPeriod sets the governance module voting period to the given duration.
+func modifyGenesisSetVotingPeriod(period time.Duration) genesisModifiers {
+	return func(gen any) (any, error) {
+		err := dyno.Set(gen, VotingPeriod.String(), "app_state", "gov", "voting_params", "voting_period")
+		return gen, err
+	}
+}
+
+// modifyGenesisICAModule sets the params of ICA module.
+func modifyGenesisICAModule(enabled bool, allowMsgs []string) genesisModifiers {
+	return func(gen any) (any, error) {
+		v := map[string]any{
+			"host_enabled":   enabled,
+			"allow_messages": allowMsgs,
+		}
+		err := dyno.Set(gen, v, "app_state", "interchainaccounts", "host_genesis_state", "params")
+		return gen, err
+	}
+}
