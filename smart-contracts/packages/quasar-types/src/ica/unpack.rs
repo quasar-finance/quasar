6219c7d86bf4c57dd0541f83d81196575bb2dc8b
LaurensKubat <Laurens.kubat@gmail.com>
2022-12-20 09:01:56 +0100

    add locktokens ack handling

--- smart-contracts/packages/quasar-types/src/ica/unpack.rs
+++ smart-contracts/packages/quasar-types/src/ica/unpack.rs
@@ -1,16 +1,21 @@
+use crate::{error::Error, ica::traits::Unpack};
 /// a collection of pack implementations for foreign types to be used in our ica contracts
 use cosmos_sdk_proto::Any;
-use osmosis_std::types::osmosis::gamm::v1beta1::{MsgJoinSwapExternAmountIn, MsgJoinSwapExternAmountInResponse};
+use osmosis_std::types::osmosis::{
+    gamm::v1beta1::{MsgJoinSwapExternAmountIn, MsgJoinSwapExternAmountInResponse},
+    lockup::{MsgLockTokens, MsgLockTokensResponse},
+};
 use prost::Message;
-use crate::{ica::traits::Unpack, error::Error};
-
 
 impl Unpack for MsgJoinSwapExternAmountIn {
     fn unpack(msg: Any) -> Result<Self, Error> {
         if msg.type_url != MsgJoinSwapExternAmountIn::TYPE_URL {
-            return Err(Error::UnpackInvalidTypeUrl{ expected: MsgJoinSwapExternAmountIn::TYPE_URL.to_string(), actual: msg.type_url });
+            return Err(Error::UnpackInvalidTypeUrl {
+                expected: MsgJoinSwapExternAmountIn::TYPE_URL.to_string(),
+                actual: msg.type_url,
+            });
         }
-        let val: Self = Message::decode(msg.value.as_ref())?; 
+        let val: Self = Message::decode(msg.value.as_ref())?;
         Ok(val)
     }
 }
@@ -20,9 +25,46 @@ impl Unpack for MsgJoinSwapExternAmountInResponse {
     fn unpack(msg: Any) -> Result<Self, Error> {
         // the type url is intended to MsgJoinSwapExternAmountIn
         if msg.type_url != MsgJoinSwapExternAmountIn::TYPE_URL {
-            return Err(Error::UnpackInvalidTypeUrl{ expected: MsgJoinSwapExternAmountIn::TYPE_URL.to_string(), actual: msg.type_url });
+            return Err(Error::UnpackInvalidTypeUrl {
+                expected: MsgJoinSwapExternAmountIn::TYPE_URL.to_string(),
+                actual: msg.type_url,
+            });
         }
-        let val: Self = Message::decode(msg.value.as_ref())?; 
+        let val: Self = Message::decode(msg.value.as_ref())?;
         Ok(val)
+    }
+}
+
+impl Unpack for MsgLockTokensResponse {
+    fn unpack(msg: Any) -> Result<Self, Error> {
+        if msg.type_url != MsgLockTokens::TYPE_URL {
+            return Err(Error::UnpackInvalidTypeUrl {
+                expected: MsgLockTokens::TYPE_URL.to_string(),
+                actual: msg.type_url,
+            });
+        }
+        let val: Self = Message::decode(msg.value.as_ref())?;
+        Ok(val)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use cosmwasm_std::Binary;
+    use osmosis_std::types::osmosis::lockup::MsgLockTokensResponse;
+
+    use crate::ica::{packet::AckBody, traits::Unpack};
+
+    #[test]
+    fn unpack_lock_tokens_response() {
+        // we take a raw response from Osmosis, decode it to an any, and then try to unpack the any
+        let raw =
+            Binary::from_base64("CiMKHS9vc21vc2lzLmxvY2t1cC5Nc2dMb2NrVG9rZW5zEgIIAQ==").unwrap();
+        let any = AckBody::from_bytes(raw.0.as_ref())
+            .unwrap()
+            .to_any()
+            .unwrap();
+        let resp = MsgLockTokensResponse::unpack(any).unwrap();
+        assert_eq!(resp.id, 1)
     }
 }
