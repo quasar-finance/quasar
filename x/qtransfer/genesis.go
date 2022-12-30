8d8a6e3ab7455ce6c766e632eb46ad20fff2bbe1
Ehsan-saradar <ehsan.saradar@gmail.com>
2022-12-30 16:12:32 +0330

    Copy ibc-hooks module from osmosis as qtransfer

---
+++ x/qtransfer/genesis.go
@@ -0,0 +1,81 @@
+package ibc_hooks
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/address"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	vestingtypes "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
+	"github.com/quasarlabs/quasarnode/x/qtransfer/types"
+)
+
+var WasmHookModuleAccountAddr sdk.AccAddress = address.Module(ModuleName, []byte("wasm-hook intermediary account"))
+
+func IbcHooksInitGenesis(ctx sdk.Context, ak types.AccountKeeper) {
+	err := createModuleAccount(ctx, ak, WasmHookModuleAccountAddr)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// createModuleAccount creates a module account at the provided address.
+// It overrides an account if it exists at that address, with a non-zero sequence number & pubkey
+// Contract: addr is derived from `address.Module(ModuleName, key)`
+func createModuleAccount(ctx sdk.Context, ak types.AccountKeeper, addr sdk.AccAddress) error {
+	err := canCreateModuleAccountAtAddr(ctx, ak, addr)
+	if err != nil {
+		return err
+	}
+
+	acc := ak.NewAccount(
+		ctx,
+		authtypes.NewModuleAccount(
+			authtypes.NewBaseAccountWithAddress(addr),
+			addr.String(),
+		),
+	)
+	ak.SetAccount(ctx, acc)
+	return nil
+}
+
+// CanCreateModuleAccountAtAddr tells us if we can safely make a module account at
+// a given address. By collision resistance of the address (given API safe construction),
+// the only way for an account to be already be at this address is if its claimed by the same
+// pre-image from the correct module,
+// or some SDK command breaks assumptions and creates an account at designated address.
+// This function checks if there is an account at that address, and runs some safety checks
+// to be extra-sure its not a user account (e.g. non-zero sequence, pubkey, of fore-seen account types).
+// If there is no account, or if we believe its not a user-spendable account, we allow module account
+// creation at the address.
+// else, we do not.
+//
+// TODO: This is generally from an SDK design flaw
+// code based off wasmd code: https://github.com/CosmWasm/wasmd/pull/996
+// Its _mandatory_ that the caller do the API safe construction to generate a module account addr,
+// namely, address.Module(ModuleName, {key})
+func canCreateModuleAccountAtAddr(ctx sdk.Context, ak types.AccountKeeper, addr sdk.AccAddress) error {
+	existingAcct := ak.GetAccount(ctx, addr)
+	if existingAcct == nil {
+		return nil
+	}
+	if existingAcct.GetSequence() != 0 || existingAcct.GetPubKey() != nil {
+		return fmt.Errorf("cannot create module account %s, "+
+			"due to an account at that address already existing & having sent txs", addr)
+	}
+	var overrideAccountTypes = map[reflect.Type]struct{}{
+		reflect.TypeOf(&authtypes.BaseAccount{}):                 {},
+		reflect.TypeOf(&vestingtypes.DelayedVestingAccount{}):    {},
+		reflect.TypeOf(&vestingtypes.ContinuousVestingAccount{}): {},
+		reflect.TypeOf(&vestingtypes.BaseVestingAccount{}):       {},
+		reflect.TypeOf(&vestingtypes.PeriodicVestingAccount{}):   {},
+		reflect.TypeOf(&vestingtypes.PermanentLockedAccount{}):   {},
+	}
+	if _, clear := overrideAccountTypes[reflect.TypeOf(existingAcct)]; clear {
+		return nil
+	}
+	return errors.New("cannot create module account %s, " +
+		"due to an account at that address already existing & not being an overrideable type")
+}
---
+++ x/qtransfer/wasm_hook.go
@@ -0,0 +1,234 @@
+package ibc_hooks
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	sdkerrors "cosmossdk.io/errors"
+	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
+	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	transfertypes "github.com/cosmos/ibc-go/v5/modules/apps/transfer/types"
+	channeltypes "github.com/cosmos/ibc-go/v5/modules/core/04-channel/types"
+	ibcexported "github.com/cosmos/ibc-go/v5/modules/core/exported"
+	"github.com/quasarlabs/quasarnode/x/qtransfer/types"
+)
+
+type ContractAck struct {
+	ContractResult []byte `json:"contract_result"`
+	IbcAck         []byte `json:"ibc_ack"`
+}
+
+type WasmHooks struct {
+	ContractKeeper *wasmkeeper.PermissionedKeeper
+}
+
+func NewWasmHooks(contractKeeper *wasmkeeper.PermissionedKeeper) WasmHooks {
+	return WasmHooks{ContractKeeper: contractKeeper}
+}
+
+func (h WasmHooks) OnRecvPacketOverride(im IBCMiddleware, ctx sdk.Context, packet channeltypes.Packet, relayer sdk.AccAddress) ibcexported.Acknowledgement {
+	if h.ContractKeeper == nil {
+		// Not configured
+		return im.App.OnRecvPacket(ctx, packet, relayer)
+	}
+
+	isIcs20, data := isIcs20Packet(packet)
+	if !isIcs20 {
+		return im.App.OnRecvPacket(ctx, packet, relayer)
+	}
+
+	// Validate the memo
+	isWasmRouted, contractAddr, msgBytes, err := ValidateAndParseMemo(data.GetMemo(), data.Receiver)
+	if !isWasmRouted {
+		return im.App.OnRecvPacket(ctx, packet, relayer)
+	}
+	if err != nil {
+		return channeltypes.NewErrorAcknowledgement(err)
+	}
+	if msgBytes == nil || contractAddr == nil { // This should never happen
+		return channeltypes.NewErrorAcknowledgement(errors.New("error in wasmhook message validation"))
+	}
+
+	// The funds sent on this packet need to be transferred to the wasm hooks module address/
+	// For this, we override the ICS20 packet's Receiver (essentially hijacking the funds for the module)
+	// and execute the underlying OnRecvPacket() call (which should eventually land on the transfer app's
+	// relay.go and send the sunds to the module.
+	//
+	// If that succeeds, we make the contract call
+	data.Receiver = WasmHookModuleAccountAddr.String()
+	bz, err := json.Marshal(data)
+	if err != nil {
+		return channeltypes.NewErrorAcknowledgement(fmt.Errorf("cannot marshal the ICS20 packet: %w", err))
+	}
+	packet.Data = bz
+
+	// Execute the receive
+	ack := im.App.OnRecvPacket(ctx, packet, relayer)
+	if !ack.Success() {
+		return ack
+	}
+
+	amount, ok := sdk.NewIntFromString(data.GetAmount())
+	if !ok {
+		// This should never happen, as it should've been caught in the underlaying call to OnRecvPacket,
+		// but returning here for completeness
+		return channeltypes.NewErrorAcknowledgement(fmt.Errorf("invalid packet data: Amount is not an int"))
+	}
+
+	// The packet's denom is the denom in the sender chain. This needs to be converted to the local denom.
+	denom := MustExtractDenomFromPacketOnRecv(packet)
+	funds := sdk.NewCoins(sdk.NewCoin(denom, amount))
+
+	execMsg := wasmtypes.MsgExecuteContract{
+		Sender:   WasmHookModuleAccountAddr.String(),
+		Contract: contractAddr.String(),
+		Msg:      msgBytes,
+		Funds:    funds,
+	}
+	response, err := h.execWasmMsg(ctx, &execMsg)
+	if err != nil {
+		return channeltypes.NewErrorAcknowledgement(err)
+	}
+
+	fullAck := ContractAck{ContractResult: response.Data, IbcAck: ack.Acknowledgement()}
+	bz, err = json.Marshal(fullAck)
+	if err != nil {
+		return channeltypes.NewErrorAcknowledgement(sdkerrors.Wrap(err, "cannot marshal the contract acknowledgement"))
+	}
+
+	return channeltypes.NewResultAcknowledgement(bz)
+}
+
+// MustExtractDenomFromPacketOnRecv takes a packet with a valid ICS20 token data in the Data field and returns the
+// denom as represented in the local chain.
+// If the data cannot be unmarshalled this function will panic
+func MustExtractDenomFromPacketOnRecv(packet ibcexported.PacketI) string {
+	var data transfertypes.FungibleTokenPacketData
+	if err := json.Unmarshal(packet.GetData(), &data); err != nil {
+		panic("unable to unmarshal ICS20 packet data")
+	}
+
+	var denom string
+	if transfertypes.ReceiverChainIsSource(packet.GetSourcePort(), packet.GetSourceChannel(), data.Denom) {
+		// remove prefix added by sender chain
+		voucherPrefix := transfertypes.GetDenomPrefix(packet.GetSourcePort(), packet.GetSourceChannel())
+
+		unprefixedDenom := data.Denom[len(voucherPrefix):]
+
+		// coin denomination used in sending from the escrow address
+		denom = unprefixedDenom
+
+		// The denomination used to send the coins is either the native denom or the hash of the path
+		// if the denomination is not native.
+		denomTrace := transfertypes.ParseDenomTrace(unprefixedDenom)
+		if denomTrace.Path != "" {
+			denom = denomTrace.IBCDenom()
+		}
+	} else {
+		prefixedDenom := transfertypes.GetDenomPrefix(packet.GetDestPort(), packet.GetDestChannel()) + data.Denom
+		denom = transfertypes.ParseDenomTrace(prefixedDenom).IBCDenom()
+	}
+	return denom
+}
+
+func (h WasmHooks) execWasmMsg(ctx sdk.Context, execMsg *wasmtypes.MsgExecuteContract) (*wasmtypes.MsgExecuteContractResponse, error) {
+	if err := execMsg.ValidateBasic(); err != nil {
+		return nil, fmt.Errorf(types.ErrBadExecutionMsg, err.Error())
+	}
+	wasmMsgServer := wasmkeeper.NewMsgServerImpl(h.ContractKeeper)
+	return wasmMsgServer.ExecuteContract(sdk.WrapSDKContext(ctx), execMsg)
+}
+
+func isIcs20Packet(packet channeltypes.Packet) (isIcs20 bool, ics20data transfertypes.FungibleTokenPacketData) {
+	var data transfertypes.FungibleTokenPacketData
+	if err := json.Unmarshal(packet.GetData(), &data); err != nil {
+		return false, data
+	}
+	return true, data
+}
+
+func isMemoWasmRouted(memo string) (isWasmRouted bool, metadata map[string]interface{}) {
+	metadata = make(map[string]interface{})
+
+	// If there is no memo, the packet was either sent with an earlier version of IBC, or the memo was
+	// intentionally left blank. Nothing to do here. Ignore the packet and pass it down the stack.
+	if len(memo) == 0 {
+		return false, metadata
+	}
+
+	// the metadata must be a valid JSON object
+	err := json.Unmarshal([]byte(memo), &metadata)
+	if err != nil {
+		return false, metadata
+	}
+
+	// If the key "wasm" doesn't exist, there's nothing to do on this hook. Continue by passing the packet
+	// down the stack
+	_, ok := metadata["wasm"]
+	if !ok {
+		return false, metadata
+	}
+
+	return true, metadata
+}
+
+func ValidateAndParseMemo(memo string, receiver string) (isWasmRouted bool, contractAddr sdk.AccAddress, msgBytes []byte, err error) {
+	isWasmRouted, metadata := isMemoWasmRouted(memo)
+	if !isWasmRouted {
+		return isWasmRouted, sdk.AccAddress{}, nil, nil
+	}
+
+	wasmRaw := metadata["wasm"]
+
+	// Make sure the wasm key is a map. If it isn't, ignore this packet
+	wasm, ok := wasmRaw.(map[string]interface{})
+	if !ok {
+		return isWasmRouted, sdk.AccAddress{}, nil,
+			fmt.Errorf(types.ErrBadMetadataFormatMsg, memo, "wasm metadata is not a valid JSON map object")
+	}
+
+	// Get the contract
+	contract, ok := wasm["contract"].(string)
+	if !ok {
+		// The tokens will be returned
+		return isWasmRouted, sdk.AccAddress{}, nil,
+			fmt.Errorf(types.ErrBadMetadataFormatMsg, memo, `Could not find key wasm["contract"]`)
+	}
+
+	contractAddr, err = sdk.AccAddressFromBech32(contract)
+	if err != nil {
+		return isWasmRouted, sdk.AccAddress{}, nil,
+			fmt.Errorf(types.ErrBadMetadataFormatMsg, memo, `wasm["contract"] is not a valid bech32 address`)
+	}
+
+	// The contract and the receiver should be the same for the packet to be valid
+	if contract != receiver {
+		return isWasmRouted, sdk.AccAddress{}, nil,
+			fmt.Errorf(types.ErrBadMetadataFormatMsg, memo, `wasm["contract"] should be the same as the receiver of the packet`)
+	}
+
+	// Ensure the message key is provided
+	if wasm["msg"] == nil {
+		return isWasmRouted, sdk.AccAddress{}, nil,
+			fmt.Errorf(types.ErrBadMetadataFormatMsg, memo, `Could not find key wasm["msg"]`)
+	}
+
+	// Make sure the msg key is a map. If it isn't, return an error
+	_, ok = wasm["msg"].(map[string]interface{})
+	if !ok {
+		return isWasmRouted, sdk.AccAddress{}, nil,
+			fmt.Errorf(types.ErrBadMetadataFormatMsg, memo, `wasm["msg"] is not a map object`)
+	}
+
+	// Get the message string by serializing the map
+	msgBytes, err = json.Marshal(wasm["msg"])
+	if err != nil {
+		// The tokens will be returned
+		return isWasmRouted, sdk.AccAddress{}, nil,
+			fmt.Errorf(types.ErrBadMetadataFormatMsg, memo, err.Error())
+	}
+
+	return isWasmRouted, contractAddr, msgBytes, nil
+}
