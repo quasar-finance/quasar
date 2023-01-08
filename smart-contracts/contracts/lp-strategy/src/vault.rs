77f9d2d419cf01cdc72578e90500024b45f23b0c
LaurensKubat <32776056+LaurensKubat@users.noreply.github.com>
2023-01-08 13:43:01 +0100

    cleanup and refactor

--- smart-contracts/contracts/lp-strategy/src/vault.rs
+++ smart-contracts/contracts/lp-strategy/src/vault.rs
@@ -1,12 +1,29 @@
-use cosmwasm_std::{Coin, Uint128, Response};
+use osmosis_std::types::cosmos::bank::v1beta1::QueryBalanceRequest;
+use prost::Message;
+use cosmwasm_std::{Coin, Uint128, Response, CosmosMsg, Storage};
+use quasar_types::{icq::{Query, InterchainQueryPacketData}};
 
-use crate::{error::ContractError, state::Claim};
+use crate::{error::ContractError, state::{Claim, CHANNELS, CONFIG}, helpers::get_ica_address};
 
 
 
 pub fn deposit(funds: Vec<Coin>) -> Result<Response, ContractError> {
     // deposit need to internally rebuild the amount of funds under the smart contract, can this be just deposited + already autocompounded?
     todo!()
+}
+
+
+pub fn prepare_icq_balance(storage: &dyn Storage, channel: String) -> Result<InterchainQueryPacketData, ContractError>{
+    let address = get_ica_address(storage, channel)?;
+
+    let denom = CONFIG.load(storage)?.denom;
+    let query = QueryBalanceRequest { address, denom };
+    Ok(Query::new()
+        .add_request(
+            query.encode_to_vec(),
+            "/cosmos.bank.v1beta1.Query/Balance".into(),
+        )
+        .encode_pkt())
 }
 
 // create_claim 
