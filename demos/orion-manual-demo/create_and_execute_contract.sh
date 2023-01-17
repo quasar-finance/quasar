0a46ba9196307d0a975fcfcc500554b6e5f66cce
LaurensKubat <32776056+LaurensKubat@users.noreply.github.com>
2023-01-17 11:01:54 +0100

    update test script

--- demos/orion-manual-demo/create_and_execute_contract.sh
+++ demos/orion-manual-demo/create_and_execute_contract.sh
@@ -13,7 +13,7 @@ echo $NODE
 #     base_denom: base_denom should be the denom of the token on osmosos, for now uosmo
 #     local_denom: the denom of the token used locally, in this testing case: the denom of the path transfer/channel-1/uosmo
 #     quote_denom is the denom other denom in the pool, stake for now
-INIT='{"lock_period":"1209600","pool_id":1,"pool_denom":"gamm/pool/1","base_denom":"uosmo","base_denom":"ibc/0471F1C4E7AFD3F07702BEF6DC365268D64570F7C1FDC98EA6098DD6DE59817B","quote_denom":"stake"}'
+INIT='{"lock_period":"1209600","pool_id":1,"pool_denom":"gamm/pool/1","base_denom":"uosmo","local_denom":"ibc/0471F1C4E7AFD3F07702BEF6DC365268D64570F7C1FDC98EA6098DD6DE59817B","quote_denom":"stake"}'
 
 cd ../../smart-contracts
 
