a18ec35ed144ad68909595e6d7e725ea67804218
LaurensKubat <32776056+LaurensKubat@users.noreply.github.com>
2023-02-21 16:00:44 +0100

    clean up orion demo
    Signed-off-by: njerschow <n@quasar.fi>

--- demos/orion-manual-demo/run_all.sh
+++ demos/orion-manual-demo/run_all.sh
@@ -36,7 +36,9 @@ sleep 10
 
 # create a pool on osmosis to test against
 osmosisd tx gamm create-pool --pool-file ./sample_pool1.json --node http://127.0.0.1:26679 --from bob --keyring-backend test --home $HOME/.osmosis --chain-id osmosis -y
+sleep 6
 osmosisd tx gamm create-pool --pool-file ./sample_pool2.json --node http://127.0.0.1:26679 --from bob --keyring-backend test --home $HOME/.osmosis --chain-id osmosis -y
+sleep 6
 osmosisd tx gamm create-pool --pool-file ./sample_pool3.json --node http://127.0.0.1:26679 --from bob --keyring-backend test --home $HOME/.osmosis --chain-id osmosis -y
 
 # run hermes and save pid, run_hermes and setup_go_relayer might not relay over the same channel out of the box due to connection creation in both scripts
