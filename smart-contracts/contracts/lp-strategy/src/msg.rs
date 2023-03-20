23595184398a52a6b03cc1c33e9407abfd2abfb5
LaurensKubat <32776056+LaurensKubat@users.noreply.github.com>
2023-03-20 23:19:02 +0100

    stable

--- smart-contracts/contracts/lp-strategy/src/msg.rs
+++ smart-contracts/contracts/lp-strategy/src/msg.rs
@@ -15,11 +15,12 @@ use crate::{
 #[cw_serde]
 pub struct InstantiateMsg {
     pub lock_period: u64,
-    pub pool_id: u64,
-    pub pool_denom: String,
-    pub local_denom: String,
-    pub base_denom: String,
-    pub quote_denom: String,
+    pub pool_id: u64, // 2
+    pub pool_denom: String, // gamm/pool/2
+    // if setup correctly, local_denom on quasar == base_denom on osmosis
+    pub local_denom: String, // ibc/ED07
+    pub base_denom: String, // uosmo
+    pub quote_denom: String, // uatom
     // TODO should this be outgoing_transfer_channel?
     pub transfer_channel: String,
     // TODO rename to return_transfer_channel
