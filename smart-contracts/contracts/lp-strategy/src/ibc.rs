9df28ca564b780915a0c7c009c6e5d76f06c253e
LaurensKubat <32776056+LaurensKubat@users.noreply.github.com>
2023-03-23 08:43:35 +0100

    add forgotten ?

--- smart-contracts/contracts/lp-strategy/src/ibc.rs
+++ smart-contracts/contracts/lp-strategy/src/ibc.rs
@@ -389,7 +389,7 @@ pub fn handle_icq_ack(
     let actual = get_usable_bond_balance(storage, &querier, &env, &config)?;
 
     TOTAL_VAULT_BALANCE.save(storage, &total_balance)?;
-    let scaled = if actual == Uint128::zero() || SIMULATED_JOIN_RESULT.load(storage) == Uint128::zero() {
+    let scaled = if actual == Uint128::zero() || SIMULATED_JOIN_RESULT.load(storage)? == Uint128::zero() {
         SIMULATED_JOIN_RESULT.load(storage)?
     } else {
         scale_join_pool(storage, actual, join_pool)?
