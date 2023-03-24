d77be49a9243cc01eeaa6954af716f711489022a
LaurensKubat <32776056+LaurensKubat@users.noreply.github.com>
2023-03-24 20:21:09 +0100

    add contract error

--- smart-contracts/contracts/lp-strategy/src/helpers.rs
+++ smart-contracts/contracts/lp-strategy/src/helpers.rs
@@ -177,16 +177,16 @@ pub fn is_contract_admin(
     querier: &QuerierWrapper,
     env: &Env,
     sus_admin: &Addr,
-) -> Result<(), VaultRewardsError> {
+) -> Result<(), ContractError> {
     let contract_admin = querier
         .query_wasm_contract_info(&env.contract.address)?
         .admin;
     if let Some(contract_admin) = contract_admin {
         if contract_admin != *sus_admin {
-            return Err(VaultRewardsError::Unauthorized {});
+            return Err(ContractError::Unauthorized {});
         }
     } else {
-        return Err(VaultRewardsError::Unauthorized {});
+        return Err(ContractError::Unauthorized {});
     }
     Ok(())
 }
