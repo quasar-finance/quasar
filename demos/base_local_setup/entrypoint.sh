4bc89bb97a536bc1163a9db24435411746544f6a
Pablo <118397961+pabloqsr@users.noreply.github.com>
2023-04-11 01:30:48 +0200

    Move intialization flag to home dir

--- demos/base_local_setup/entrypoint.sh
+++ demos/base_local_setup/entrypoint.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 # This script checks if the container is started for the first time.
 
+set -e  
 CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
-ls /tmp
 
-if [ ! -e /tmp/$CONTAINER_FIRST_STARTUP ]; then
-    touch /tmp/$CONTAINER_FIRST_STARTUP
+if [ ! -e $CONTAINER_FIRST_STARTUP ]; then
     echo "Initializing the container config"
+    touch $CONTAINER_FIRST_STARTUP
     src/quasar/demos/base_local_setup/quasar_localnet.sh
 else
     echo "Node already initialized"
     
 fi
 
-wait 20
 quasarnoded start 
