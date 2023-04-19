7828b1b23070e6a0475d453174fa03b86552b568
0xMagio <31893902+magiodev@users.noreply.github.com>
2023-04-19 14:44:44 +0200

    Update upgrade_handler_v0_1_1.sh script to test upgradeHandler

--- demos/upgrade-handler/upgrade_handler_v0_1_1.sh
+++ demos/upgrade-handler/upgrade_handler_v0_1_1.sh
@@ -9,19 +9,24 @@ pkill quasarnoded || true
 
 # Define variables
 CHAIN_ID=quasar
-UPGRADE_HEIGHT=100
+UPGRADE_HEIGHT=30
 
 #TODO run this script as main branch, pre build the new binary from the new branch and set it up manually on the cosmovisor folder
+#TODO git checkout v0.1.0
+#TODO make install
+
+echo ">>> Sleeping 10 seconds to create some initial blocks"
+sleep 10
 
 # Submit governance proposal for software-upgrade to v0.1.1
 echo ">>> Submitting proposal for software-upgrade"
-PROPOSAL_ID=$(quasarnoded tx gov submit-proposal software-upgrade "v0.1.1" --title "Software Upgrade to v0.1.1" --description "This software-upgrade v0.1.1 introduces QVesting module for continuous vesting schedule accounts creation" --upgrade-height $UPGRADE_HEIGHT --deposit 1uqsr --from my_treasury --chain-id $CHAIN_ID --keyring-backend test -y | jq -r '.logs[0].events[0].attributes[0].value')
+quasarnoded tx gov submit-proposal software-upgrade "v0.1.1" --title "Software Upgrade to v0.1.1" --description "This software-upgrade v0.1.1 introduces QVesting module for continuous vesting schedule accounts creation" --upgrade-height $UPGRADE_HEIGHT --deposit 1uqsr --from my_treasury --chain-id $CHAIN_ID --keyring-backend test -y
 
-echo ">>> Sleeping 5 seconds after submitting proposal"
-sleep 5
+echo ">>> Sleeping 60 seconds after submitting proposal"
+sleep 60
 
 echo ">>> Voting yes to proposal"
-quasarnoded tx gov vote $PROPOSAL_ID yes --from my_treasury --chain-id $CHAIN_ID --keyring-backend test -y
+quasarnoded tx gov vote 1 yes --from my_treasury --chain-id $CHAIN_ID --keyring-backend test -y
 
 echo ">>> Sleeping 5 seconds after voting proposal"
 sleep 5
@@ -30,10 +35,11 @@ sleep 5
 echo ">>> Waiting for the block height to reach $UPGRADE_HEIGHT"
 while true; do
   CURRENT_HEIGHT=$(quasarnoded status | jq -r '.SyncInfo.latest_block_height')
+  echo "Current height: "$CURRENT_HEIGHT
   if [ "$CURRENT_HEIGHT" -ge "$UPGRADE_HEIGHT" ]; then
     break
   fi
-  sleep 5
+  sleep 1
 done
 
 # Check if the upgrade has been successful
