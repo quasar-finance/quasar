d496b00af110f5df071dfadca9fbe26c12f98554
Nikita (rizz) <nj18@nyu.edu>
2023-04-27 14:21:58 -0400

    fmt jeez

--- smart-contracts/contracts/lp-strategy/src/helpers.rs
+++ smart-contracts/contracts/lp-strategy/src/helpers.rs
@@ -4,8 +4,8 @@ use crate::{
     ibc_lock::Lock,
     msg::ExecuteMsg,
     state::{
-        PendingBond, PendingSingleUnbond, RawAmount, CHANNELS, CONFIG, IBC_LOCK,
-        REPLIES, SHARES, TRAPS,
+        PendingBond, PendingSingleUnbond, RawAmount, CHANNELS, CONFIG, IBC_LOCK, REPLIES, SHARES,
+        TRAPS,
     },
     unbond::PendingReturningUnbonds,
 };
