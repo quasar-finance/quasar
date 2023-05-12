aaf8efa70b4af07e15daa63003e2a4e94312b044
magiodev <31893902+magiodev@users.noreply.github.com>
2023-05-12 16:58:18 +0200

    Remove unused function

--- x/qvesting/keeper/keeper.go
+++ x/qvesting/keeper/keeper.go
@@ -2,7 +2,6 @@ package keeper
 
 import (
 	"fmt"
-	"github.com/cosmos/cosmos-sdk/x/auth/vesting/exported"
 	"github.com/tendermint/tendermint/libs/log"
 
 	"github.com/cosmos/cosmos-sdk/codec"
@@ -51,28 +50,6 @@ func NewKeeper(
 func (k Keeper) AddVestingAccount(ctx sdk.Context, addr sdk.AccAddress) {
 	store := ctx.KVStore(k.storeKey)
 	store.Set(types.VestingAccountStoreKey(addr), []byte{})
-}
-
-// IterateVestingAccounts iterates over all the stored accounts and performs a callback function.
-// Stops iteration when callback returns true.
-func (k Keeper) IterateVestingAccounts(ctx sdk.Context, cb func(account exported.VestingAccount) (stop bool)) {
-	store := ctx.KVStore(k.storeKey)
-	iterator := sdk.KVStorePrefixIterator(store, types.VestingAccountStoreKeyPrefix)
-
-	defer iterator.Close()
-	for ; iterator.Valid(); iterator.Next() {
-		addr := types.AddressFromVestingAccountKey(iterator.Key())
-
-		acct := k.accountKeeper.GetAccount(ctx, addr)
-		vestingAcct, ok := acct.(exported.VestingAccount)
-		if !ok {
-			// not vesting account
-			continue
-		}
-		if cb(vestingAcct) {
-			break
-		}
-	}
 }
 
 func (k Keeper) Logger(ctx sdk.Context) log.Logger {
