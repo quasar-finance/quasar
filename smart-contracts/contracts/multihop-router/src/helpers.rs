adcba819db54f2736093d08ef36eb0c77d62ed5e
LaurensKubat <32776056+LaurensKubat@users.noreply.github.com>
2023-05-17 19:24:43 +0200

    add is_contract_admin

--- smart-contracts/contracts/multihop-router/src/helpers.rs
+++ smart-contracts/contracts/multihop-router/src/helpers.rs
@@ -1,9 +1,9 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::{to_binary, Addr, CosmosMsg, StdResult, WasmMsg};
+use cosmwasm_std::{to_binary, Addr, CosmosMsg, Env, QuerierWrapper, StdResult, WasmMsg};
 
-use crate::msg::ExecuteMsg;
+use crate::{msg::ExecuteMsg, ContractError};
 
 /// CwTemplateContract is a wrapper around Addr that provides a lot of helpers
 /// for working with this.
@@ -24,4 +24,22 @@ impl CwTemplateContract {
         }
         .into())
     }
+}
+
+pub fn is_contract_admin(
+    querier: &QuerierWrapper,
+    env: &Env,
+    sus_admin: &Addr,
+) -> Result<(), ContractError> {
+    let contract_admin = querier
+        .query_wasm_contract_info(&env.contract.address)?
+        .admin;
+    if let Some(contract_admin) = contract_admin {
+        if contract_admin != *sus_admin {
+            return Err(ContractError::Unauthorized {});
+        }
+    } else {
+        return Err(ContractError::Unauthorized {});
+    }
+    Ok(())
 }
