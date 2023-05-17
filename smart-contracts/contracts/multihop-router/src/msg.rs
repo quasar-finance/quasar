53d8e23793a13f4e90536c32a9bffffec9d5014e
LaurensKubat <32776056+LaurensKubat@users.noreply.github.com>
2023-05-17 14:06:07 +0200

    add remove route

--- smart-contracts/contracts/multihop-router/src/msg.rs
+++ smart-contracts/contracts/multihop-router/src/msg.rs
@@ -1,40 +1,32 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
 
-use crate::state::{Hop, Memo, Destination};
+use crate::state::{Destination, Hop, Memo};
 
 #[cw_serde]
 pub struct InstantiateMsg {}
 
 #[cw_serde]
 pub enum ExecuteMsg {
-    AddRoute {
-        hop: Hop,
-    },
-    MutateRoute {
-        destination: String
-    },
+    AddRoute { hop: Hop },
+    MutateRoute { destination: String },
+    RemoveRoute { destination: String },
 }
 
 #[cw_serde]
 #[derive(QueryResponses)]
 pub enum QueryMsg {
     #[returns(GetMemoResponse)]
-    GetMemo{
-        destination: String
-    },
+    GetMemo { destination: String },
     #[returns(GetRouteResponse)]
-    GetRoute{
-        destination: String
-    },
+    GetRoute { destination: String },
     #[returns(ListRoutesResponse)]
-    ListRoutes{},
+    ListRoutes {},
 }
 
 #[cw_serde]
 pub struct GetMemoResponse {
     memo: Memo,
 }
-
 
 #[cw_serde]
 pub struct GetRouteResponse {
@@ -43,5 +35,5 @@ pub struct GetRouteResponse {
 
 #[cw_serde]
 pub struct ListRoutesResponse {
-    memos: Vec<(Destination, Hop)>
+    memos: Vec<(Destination, Hop)>,
 }
