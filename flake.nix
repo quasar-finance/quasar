ec2b7697fd43499fea089205ff1cc1679b82870a
LaurensKubat <32776056+LaurensKubat@users.noreply.github.com>
2023-06-01 12:51:09 +0200

    add initial flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "The Quasar development flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+
+  outputs = { self, nixpkgs, flake-utils}: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.cargo
+            pkgs.rustc
+            pkgs.go_1_18
+            pkgs.gotools
+            pkgs.golangci-lint
+            pkgs.gopls
+            pkgs.go-outline
+            pkgs.gopkgs
+          ];
+        };
+      });
+}
