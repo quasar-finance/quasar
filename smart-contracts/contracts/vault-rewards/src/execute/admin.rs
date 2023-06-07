78ce4270f0d6ae0719379267cc880019b07f35f1
Nikita (rizz) <nj18@nyu.edu>
2023-06-07 10:57:10 -0400

    fix lint

--- smart-contracts/contracts/vault-rewards/src/execute/admin.rs
+++ smart-contracts/contracts/vault-rewards/src/execute/admin.rs
@@ -11,7 +11,7 @@ pub fn execute_withdraw_funds(
     mut asset: Asset,
 ) -> Result<Response, VaultRewardsError> {
     let config = CONFIG.load(deps.storage)?;
-    let reward_token = config.reward_token;
+    let reward_token = config.reward_token.clone();
 
     if asset.info == reward_token {
         // check if reward balance is sufficient after withdrawal
