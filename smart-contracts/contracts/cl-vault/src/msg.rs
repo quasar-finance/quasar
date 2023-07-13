f5bb717d04141fed957b33dd429db1e31d9f9e5d
Nikita (rizz) <nj18@nyu.edu>
2023-07-13 13:55:15 +0200

    push wip

--- smart-contracts/contracts/cl-vault/src/msg.rs
+++ smart-contracts/contracts/cl-vault/src/msg.rs
@@ -1,11 +1,30 @@
-use cosmwasm_schema::{cw_serde, QueryResponses};
+use apollo_vault::msg::{ExtensionExecuteMsg, ExtensionQueryMsg};
+use apollo_vault::state::ConfigUnchecked;
+use cosmwasm_schema::cw_serde;
+use cw_vault_standard::{VaultStandardExecuteMsg, VaultStandardQueryMsg};
+
+/// ExecuteMsg for an Autocompounding Vault.
+pub type ExecuteMsg = VaultStandardExecuteMsg<ExtensionExecuteMsg>;
+
+/// QueryMsg for an Autocompounding Vault.
+pub type QueryMsg = VaultStandardQueryMsg<ExtensionQueryMsg>;
 
 #[cw_serde]
-pub struct InstantiateMsg {}
+pub struct InstantiateMsg {
+    /// Address that is allowed to update config.
+    pub admin: String,
+    /// The ID of the pool that this vault will autocompound.
+    pub pool_id: u64,
+    /// The lockup duration in seconds that this vault will use when staking
+    /// LP tokens.
+    pub lockup_duration: u64,
+    /// Configurable parameters for the contract.
+    pub config: ConfigUnchecked,
+    /// The subdenom that will be used for the native vault token, e.g.
+    /// the denom of the vault token will be:
+    /// "factory/{vault_contract}/{vault_token_subdenom}".
+    pub vault_token_subdenom: String,
+}
 
 #[cw_serde]
-pub enum ExecuteMsg {}
-
-#[cw_serde]
-#[derive(QueryResponses)]
-pub enum QueryMsg {}
+pub struct MigrateMsg {}
