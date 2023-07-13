e15cfa5d2fdd0400353213932a8be56a46d9e12e
LaurensKubat <32776056+LaurensKubat@users.noreply.github.com>
2023-07-13 12:25:59 +0200

    add liquidity math

--- smart-contracts/contracts/cl-vault/src/error.rs
+++ smart-contracts/contracts/cl-vault/src/error.rs
@@ -1,10 +1,16 @@
-use cosmwasm_std::StdError;
+use cosmwasm_std::{StdError, OverflowError, CheckedFromRatioError};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
+
+    #[error("{0}")]
+    OverflowError(#[from] OverflowError),
+
+    #[error("{0}")]
+    CheckedFromRatioError(#[from] CheckedFromRatioError),
 
     #[error("Unauthorized")]
     Unauthorized {},
