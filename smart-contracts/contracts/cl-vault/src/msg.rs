f0118d42f3c2f80ce7bd7ad361d547a681c1be6d
Nikita (rizz) <nj18@nyu.edu>
2023-07-13 15:33:36 +0200

    wip

--- smart-contracts/contracts/cl-vault/src/msg.rs
+++ smart-contracts/contracts/cl-vault/src/msg.rs
@@ -1,7 +1,129 @@
-use apollo_vault::msg::{ExtensionExecuteMsg, ExtensionQueryMsg};
-use apollo_vault::state::ConfigUnchecked;
 use cosmwasm_schema::cw_serde;
+use cosmwasm_std::{to_binary, Addr, CosmosMsg, Env, StdResult, Uint128, WasmMsg};
 use cw_vault_standard::{VaultStandardExecuteMsg, VaultStandardQueryMsg};
+
+use crate::state::{ConfigUnchecked, ConfigUpdates};
+
+/// Extension execute messages for an apollo autocompounding vault
+#[cw_serde]
+pub enum ExtensionExecuteMsg {
+    /// Execute a callback message.
+    Callback(CallbackMsg),
+    /// Execute a an Apollo vault specific message.
+    Apollo(ApolloExtensionExecuteMsg),
+    /// Execute a message from the lockup extension.
+    #[cfg(feature = "lockup")]
+    Lockup(LockupExecuteMsg),
+    /// Execute a message from the force unlock extension.
+    #[cfg(feature = "force-unlock")]
+    ForceUnlock(ForceUnlockExecuteMsg),
+}
+
+/// Apollo extension messages define functionality that is part of all apollo
+/// vaults, but not part of the standard.
+#[cw_serde]
+pub enum ApolloExtensionExecuteMsg {
+    /// Update the configuration of the vault.
+    UpdateConfig {
+        /// The config updates.
+        updates: ConfigUpdates,
+    },
+    /// Update the vault admin.
+    UpdateAdmin {
+        /// The new admin address.
+        address: String,
+    },
+    /// Accept the admin transfer. This must be called by the new admin to
+    /// finalize the transfer.
+    AcceptAdminTransfer {},
+    /// Removes the initiated admin transfer. This can only be called by the
+    /// admin who initiated the admin transfer.
+    DropAdminTransfer {},
+}
+
+/// Apollo extension queries define functionality that is part of all apollo
+/// vaults, but not part of the standard.
+#[cw_serde]
+pub enum ApolloExtensionQueryMsg {
+    /// Query the current state of the vault.
+    State {},
+}
+
+/// Extension query messages for an apollo autocompounding vault
+#[cw_serde]
+pub enum ExtensionQueryMsg {
+    /// Queries related to the lockup extension.
+    #[cfg(feature = "lockup")]
+    Lockup(LockupQueryMsg),
+    /// Apollo extension queries.
+    Apollo(ApolloExtensionQueryMsg),
+}
+
+/// Callback messages for the autocompounding vault `Callback` extension
+#[cw_serde]
+pub enum CallbackMsg {
+    /// Sell all the rewards in the contract to the underlying tokens of the
+    /// pool.
+    SellRewards {},
+    /// Provide liquidity with all the underlying tokens of the pool currently
+    /// in the contract.
+    ProvideLiquidity {},
+    /// Stake all base tokens in the contract.
+    Stake {
+        /// Contract base token balance before this transaction started. E.g. if
+        /// funds were sent to the contract as part of the `info.funds` or
+        /// received as cw20s in a previous message they must be deducted from
+        /// the current contract balance.
+        base_token_balance_before: Uint128,
+    },
+    /// Mint vault tokens
+    MintVaultToken {
+        /// The amount of base tokens to deposit.
+        amount: Uint128,
+        /// The recipient of the vault token.
+        recipient: Addr,
+    },
+    /// Redeem vault tokens for base tokens.
+    #[cfg(feature = "redeem")]
+    Redeem {
+        /// The address which should receive the withdrawn base tokens.
+        recipient: Addr,
+        /// The amount of vault tokens sent to the contract. In the case that
+        /// the vault token is a Cosmos native denom, we of course have this
+        /// information in the info.funds, but if the vault implements the
+        /// Cw4626 API, then we need this argument. We figured it's
+        /// better to have one API for both types of vaults, so we
+        /// require this argument.
+        amount: Uint128,
+    },
+    /// Burn vault tokens and start the unlocking process.
+    #[cfg(feature = "lockup")]
+    Unlock {
+        /// The address that will be the owner of the unlocking position.
+        owner: Addr,
+        /// The amount of vault tokens to burn.
+        vault_token_amount: Uint128,
+    },
+    /// Save the currently pending claim to the `claims` storage.
+    #[cfg(feature = "lockup")]
+    SaveClaim {},
+}
+
+impl CallbackMsg {
+    /// Convert the callback message to a [`CosmosMsg`]. The message will be
+    /// formatted as a `Callback` extension in a [`VaultStandardExecuteMsg`],
+    /// accordning to the
+    /// [CosmWasm Vault Standard](https://docs.rs/cosmwasm-vault-standard/0.1.0/cosmwasm_vault_standard/#how-to-use-extensions).
+    pub fn into_cosmos_msg(&self, env: &Env) -> StdResult<CosmosMsg> {
+        Ok(CosmosMsg::Wasm(WasmMsg::Execute {
+            contract_addr: env.contract.address.to_string(),
+            msg: to_binary(&VaultStandardExecuteMsg::VaultExtension(
+                ExtensionExecuteMsg::Callback(self.clone()),
+            ))?,
+            funds: vec![],
+        }))
+    }
+}
 
 /// ExecuteMsg for an Autocompounding Vault.
 pub type ExecuteMsg = VaultStandardExecuteMsg<ExtensionExecuteMsg>;
--- smart-contracts/contracts/cl-vault/src/state.rs
+++ smart-contracts/contracts/cl-vault/src/state.rs
@@ -1,1 +1,130 @@
+use apollo_cw_asset::{AssetInfo, AssetInfoBase};
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::{
+    Addr, BlockInfo, Decimal, Deps, MessageInfo, Order, StdError, StdResult, Storage, Uint128,
+};
+use cw20::Expiration;
+use cw_dex_router::helpers::CwDexRouterBase;
+use cw_storage_plus::{Bound, Index, IndexList, IndexedMap, Item, MultiIndex};
+use cw_vault_standard::extensions::lockup::UnlockingPosition;
+use derive_builder::Builder;
+use liquidity_helper::LiquidityHelperBase;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
 
+/// Base config struct for the contract.
+#[cw_serde]
+#[derive(Builder)]
+#[builder(derive(Serialize, Deserialize, Debug, PartialEq, JsonSchema))]
+pub struct ConfigBase<T> {
+    /// Percentage of profit to be charged as performance fee
+    pub performance_fee: Decimal,
+    /// Account to receive fee payments
+    pub treasury: T,
+    /// Router address
+    pub router: CwDexRouterBase<T>,
+    /// The assets that are given as liquidity mining rewards that the vault
+    /// will compound into more of base_token.
+    pub reward_assets: Vec<AssetInfoBase<T>>,
+    /// The asset to which we should swap reward_assets into before providing
+    /// liquidity. Should be one of the assets in the pool.
+    pub reward_liquidation_target: AssetInfoBase<T>,
+    /// Whitelisted addresses that can call ForceWithdraw and
+    /// ForceWithdrawUnlocking
+    pub force_withdraw_whitelist: Vec<T>,
+    /// Helper for providing liquidity with unbalanced assets.
+    pub liquidity_helper: LiquidityHelperBase<T>,
+}
+
+/// Config with non-validated addresses.
+pub type ConfigUnchecked = ConfigBase<String>;
+/// Config with validated addresses.
+pub type Config = ConfigBase<Addr>;
+/// Config updates struct containing same fields as Config, but all fields are
+/// optional.
+pub type ConfigUpdates = ConfigBaseBuilder<String>;
+
+/// Merges the old config with a new partial config.
+impl Config {
+    /// Updates the existing config with the new config updates. If a field is
+    /// `None` in the `updates` then the old config is kept, else it is updated
+    /// to the new value.
+    pub fn update(self, deps: Deps, updates: ConfigUpdates) -> StdResult<Config> {
+        ConfigUnchecked {
+            performance_fee: updates.performance_fee.unwrap_or(self.performance_fee),
+            treasury: updates.treasury.unwrap_or_else(|| self.treasury.into()),
+            router: updates.router.unwrap_or_else(|| self.router.into()),
+            reward_assets: updates
+                .reward_assets
+                .unwrap_or_else(|| self.reward_assets.into_iter().map(Into::into).collect()),
+            reward_liquidation_target: updates
+                .reward_liquidation_target
+                .unwrap_or_else(|| self.reward_liquidation_target.into()),
+            force_withdraw_whitelist: updates.force_withdraw_whitelist.unwrap_or_else(|| {
+                self.force_withdraw_whitelist
+                    .into_iter()
+                    .map(Into::into)
+                    .collect()
+            }),
+            liquidity_helper: updates
+                .liquidity_helper
+                .unwrap_or_else(|| self.liquidity_helper.into()),
+        }
+        .check(deps)
+    }
+}
+
+impl ConfigUnchecked {
+    /// Constructs a Config from the unchecked config, validating all addresses.
+    pub fn check(&self, deps: Deps) -> StdResult<Config> {
+        if self.performance_fee > Decimal::one() {
+            return Err(StdError::generic_err(
+                "Performance fee cannot be greater than 100%",
+            ));
+        }
+
+        let reward_assets: Vec<AssetInfo> = self
+            .reward_assets
+            .iter()
+            .map(|x| x.check(deps.api))
+            .collect::<StdResult<_>>()?;
+        let router = self.router.check(deps.api)?;
+        let reward_liquidation_target = self.reward_liquidation_target.check(deps.api)?;
+
+        // Check that the router can route between all reward assets and the
+        // reward liquidation target. We discard the actual path because we
+        // don't need it here. We just need to make sure the paths exist.
+        for asset in &reward_assets {
+            // We skip the reward liquidation target because we don't need to
+            // route to it.
+            if asset == &reward_liquidation_target {
+                continue;
+            }
+            // We map the error here because the error coming from the router is
+            // not passed along into the query error, and thus we will otherwise
+            // just see "Querier contract error" and no more information.
+            router
+                .query_path_for_pair(&deps.querier, asset, &reward_liquidation_target)
+                .map_err(|_| {
+                    StdError::generic_err(format!(
+                        "Could not read path in cw-dex-router for {:?} -> {:?}",
+                        asset, reward_liquidation_target
+                    ))
+                })?;
+        }
+
+        Ok(Config {
+            performance_fee: self.performance_fee,
+            treasury: deps.api.addr_validate(&self.treasury)?,
+            reward_assets,
+            reward_liquidation_target,
+            router,
+            force_withdraw_whitelist: self
+                .force_withdraw_whitelist
+                .iter()
+                .map(|x| deps.api.addr_validate(x))
+                .collect::<StdResult<_>>()?,
+            liquidity_helper: self.liquidity_helper.check(deps.api)?,
+        })
+    }
+}
