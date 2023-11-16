8e66cafa1caa059b70c8118049d9816209f355f9
magiodev.eth <31893902+magiodev@users.noreply.github.com>
2023-11-16 14:52:56 +0100

    reimplement saturating_sub to fix osmosis extra uosmo amount

--- smart-contracts/contracts/cl-vault/src/vault/deposit.rs
+++ smart-contracts/contracts/cl-vault/src/vault/deposit.rs
@@ -130,13 +130,14 @@ pub fn handle_deposit_create_position_reply(
         .parse::<u128>()?
         .into();
 
+    // Notice: checked_sub has been replaced with saturating_sub due to overflowing response from dex
     let refunded = (
-        current_deposit.token0_in.checked_sub(Uint128::new(
+        current_deposit.token0_in.saturating_sub(Uint128::new(
             create_deposit_position_resp.amount0.parse::<u128>()?,
-        ))?,
-        current_deposit.token1_in.checked_sub(Uint128::new(
+        )),
+        current_deposit.token1_in.saturating_sub(Uint128::new(
             create_deposit_position_resp.amount1.parse::<u128>()?,
-        ))?,
+        )),
     );
 
     // total_vault_shares.is_zero() should never be zero. This should ideally always enter the else and we are just sanity checking.
@@ -248,12 +249,13 @@ fn refund_bank_msg(
     denom0: String,
     denom1: String,
 ) -> Result<Option<(BankMsg, Vec<Attribute>)>, ContractError> {
+    // Notice: checked_sub has been replaced with saturating_sub due to overflowing response from dex
     let refund0 = current_deposit
         .token0_in
-        .checked_sub(Uint128::new(resp.amount0.parse::<u128>()?))?;
+        .saturating_sub(Uint128::new(resp.amount0.parse::<u128>()?));
     let refund1 = current_deposit
         .token1_in
-        .checked_sub(Uint128::new(resp.amount1.parse::<u128>()?))?;
+        .saturating_sub(Uint128::new(resp.amount1.parse::<u128>()?));
 
     let mut attributes: Vec<Attribute> = vec![];
     let mut coins: Vec<Coin> = vec![];
