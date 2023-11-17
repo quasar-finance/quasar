4f94e07b3b5919aee92c90c198849df2be7166cf
Ajaz Ahmed Ansari <ajansari9585@gmail.com>
2023-11-17 15:40:37 +0300

    Update smart-contracts/contracts/cl-vault/src/test_tube/range.rs

--- smart-contracts/contracts/cl-vault/src/test_tube/range.rs
+++ smart-contracts/contracts/cl-vault/src/test_tube/range.rs
@@ -265,7 +265,8 @@ mod test {
     #[test]
     #[ignore]
     fn test_swap_math_poc() {
-        let (app, _contract, _cl_pool_id, _admin) = init_test_contract( // TODO: Evaluate using default_init()
+        let (app, _contract, _cl_pool_id, _admin) = init_test_contract( 
+        // TODO: Evaluate using default_init()
             "./test-tube-build/wasm32-unknown-unknown/release/cl_vault.wasm",
             &[
                 Coin::new(1_000_000_000_000, "uatom"),
