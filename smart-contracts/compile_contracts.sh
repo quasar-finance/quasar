22d757a620cc94910d0cff213489dfb9b219f1fb
magiodev <31893902+magiodev@users.noreply.github.com>
2024-01-12 16:23:43 +0100

    bump workspace-optimizer:0.15.0

--- smart-contracts/compile_contracts.sh
+++ smart-contracts/compile_contracts.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #Compile smart contracts
-docker run --rm -v "$(pwd)":/code --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry cosmwasm/workspace-optimizer:0.12.11
+docker run --rm -v "$(pwd)":/code --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry cosmwasm/workspace-optimizer:0.15.0
