66ed5969efe5b505edf6e94a76eeed05cc208362
Laurens <32776056+LaurensKubat@users.noreply.github.com>
2024-01-12 10:49:26 +0100

    add migration message

--- smart-contracts/contracts/cl-vault/src/contract.rs
+++ smart-contracts/contracts/cl-vault/src/contract.rs
@@ -11,9 +11,10 @@ use crate::query::{
 use crate::reply::Replies;
 use crate::rewards::{
     execute_collect_rewards, execute_distribute_rewards, handle_collect_incentives_reply,
-    handle_collect_spread_rewards_reply,
+    handle_collect_spread_rewards_reply, CoinList,
 };
 
+use crate::state::{IS_DISTRIBUTING, DISTRIBUTED_REWARDS};
 use crate::vault::admin::{execute_admin, execute_build_tick_exp_cache};
 use crate::vault::claim::execute_claim_user_rewards;
 use crate::vault::deposit::{execute_exact_deposit, handle_deposit_create_position_reply};
@@ -187,6 +188,9 @@ pub fn reply(deps: DepsMut, env: Env, msg: Reply) -> Result<Response, ContractEr
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn migrate(deps: DepsMut, _env: Env, _msg: MigrateMsg) -> Result<Response, ContractError> {
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    IS_DISTRIBUTING.save(deps.storage, &false)?;
+    DISTRIBUTED_REWARDS.save(deps.storage, &CoinList::new())?;
 
     Ok(Response::new().add_attribute("migrate", "successful"))
 }
