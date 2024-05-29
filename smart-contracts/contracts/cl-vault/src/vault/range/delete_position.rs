040513992c6436259d6b6e0ab7dc03c716617da4
Laurens <32776056+LaurensKubat@users.noreply.github.com>
2024-05-29 12:30:03 -0400

    add multi range actions

---
+++ smart-contracts/contracts/cl-vault/src/vault/range/delete_position.rs
@@ -0,0 +1,29 @@
+use cosmwasm_std::{DepsMut, Env, Response};
+
+use crate::{
+    state::POSITIONS,
+    vault::concentrated_liquidity::{get_position, withdraw_from_position},
+    ContractError,
+};
+
+pub fn delete_position(
+    deps: DepsMut,
+    env: Env,
+    position_id: u64,
+) -> Result<Response, ContractError> {
+    // query the position
+    let position = get_position(&deps.querier, position_id)?;
+
+    // withdraw all funds from the position
+    let withdraw = withdraw_from_position(
+        &env,
+        position_id,
+        position.position.unwrap().liquidity.parse()?,
+    )?;
+
+    // delete the position from the contracts local state
+    // we don't want to check existence of positions in POSITIONS here, if we ever leak a position somehow, we would not be able to remove it from the map
+    POSITIONS.remove(deps.storage, position_id);
+
+    Ok(Response::new().add_message(withdraw))
+}
