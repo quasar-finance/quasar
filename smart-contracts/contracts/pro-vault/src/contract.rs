0af831e0790b61d8f59633769d4f6d073dc4adcb
akure <abhi.california@gmail.com>
2024-06-02 21:02:16 +0530

    provault - instantiate test cases.

--- smart-contracts/contracts/pro-vault/src/contract.rs
+++ smart-contracts/contracts/pro-vault/src/contract.rs
@@ -13,7 +13,7 @@ use crate::vault::query::{VaultQueryMsg, query_vault_config,
 
 use cosmwasm_std::{
     entry_point, Binary, Deps, DepsMut, Env, MessageInfo, Reply, Response, 
-    StdResult, StdError, Uint128, BankMsg,CosmosMsg,
+    StdResult, StdError, Uint128, BankMsg,CosmosMsg,Attribute
     };
   
 // TODO - 
@@ -52,7 +52,6 @@ pub fn execute(
     Ok(Response::default())
 }
 
-
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
     mut deps: DepsMut,
@@ -60,30 +59,63 @@ pub fn instantiate(
     info: MessageInfo,
     msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
+    // Ensure the admin and vault owner are the same
+    if msg.provault_config.admin != info.sender {
+        return Err(ContractError::AdminVaultOwnerMismatch {});
+    }
 
-    VAULT_OWNER.set(deps.branch(), Some(info.sender.clone()))?;
+    // Set the vault owner
+    VAULT_OWNER.set(deps.branch(), Some(info.sender.clone())).map_err(|e| {
+        ContractError::SetVaultOwnerError(e.to_string())
+    })?;
 
-
+    // Construct the config from the message
     let config = Config {
         max_deposit_cap: msg.provault_config.max_deposit_cap,
-        deposit_denom: msg.provault_config.deposit_denom,
-        share_denom: msg.provault_config.share_denom,
+        deposit_denom: msg.provault_config.deposit_denom.clone(),
+        share_denom: msg.provault_config.share_denom.clone(),
         max_strategy_inst: msg.provault_config.max_strategy_inst,
         admin: msg.provault_config.admin,
     };
 
-    VAULT_CONFIG.save(deps.storage, &config)?;
+    // Save the config to storage
+    VAULT_CONFIG.save(deps.storage, &config).map_err(|e| {
+        ContractError::SaveConfigError(e.to_string())
+    })?;
 
+    // Clone necessary parts of info before moving it
+    let info_clone = info.clone();
 
     // Initialize the vault state
     let mut vault = Vault::new();
-    let response = vault.update_state(deps, env, info, VaultRunningState::Init)?;
-    
-    // TODO - PROPER EVENT HANDLING AND ERROR PROPAGATION.
-    Ok(Response::new().add_attribute("method", "instantiate"))
-    // Ok(Response::default())
- }
- 
+    let update_state_response = vault.update_state(deps, env, 
+        info_clone, VaultRunningState::Init)
+        .map_err(|e| {
+            ContractError::UpdateVaultStateError(e.to_string())
+        })?;
+
+    // Emit events using the new function
+    Ok(emit_instantiate_events(&info, &config, update_state_response.attributes))
+}
+
+
+fn emit_instantiate_events(
+    info: &MessageInfo,
+    config: &Config,
+    update_state_attributes: Vec<Attribute>
+) -> Response {
+    Response::new()
+        .add_attribute("method", "instantiate")
+        .add_attribute("vault_owner", info.sender.to_string())
+        .add_attribute("max_deposit_cap", config.max_deposit_cap.to_string())
+        .add_attribute("deposit_denom", config.deposit_denom.clone())
+        .add_attribute("share_denom", config.share_denom.clone())
+        .add_attribute("max_strategy_inst", config.max_strategy_inst.to_string())
+        .add_attribute("admin", config.admin.to_string())
+        .add_attributes(update_state_attributes) // Merge attributes from update_state
+}
+
+
 // TODO - Query Enumsd and match to be resturctured. Like Vault config query should be in vault module,
 // strategy config query to be in strategy module. 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -99,17 +131,6 @@ pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
         }
         // Handle other queries ...
     }
-
-    /* 
-     // Create a default response message
-     let response = "Default response message";
-
-     // Convert the response message to a binary format
-     let binary_response = to_json_binary(&response)?;
- 
-     // Return the binary response
-     Ok(binary_response)
-     */
 }
  
 
@@ -193,3 +214,91 @@ fn try_exec_strategy_actions(
         .add_attribute("method", "try_exec_strategy_actions"))
 }
 
+ 
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
+    use cosmwasm_std::{attr, Addr, Response, Uint128, Uint64, SystemResult, ContractResult, Binary, Empty};
+
+    use crate::msg::InstantiateMsg;
+    use crate::vault::config::Config;
+
+    // Initialization Test #1 
+    #[test] 
+    fn proper_initialization() {
+        let mut deps = mock_dependencies();
+
+        let msg = InstantiateMsg {
+            provault_config: Config {
+                admin: "admin".to_string(),
+                max_deposit_cap: Uint128::new(1000000),
+                deposit_denom: "uosmo".to_string(),
+                share_denom: "share".to_string(),
+                max_strategy_inst: Uint64::new(10),
+            },
+            name: "test_name".to_string(),
+            thesis: "test_thesis".to_string(),
+        };
+        let info = mock_info("admin", &[]);
+
+        // Try instantiating the contract
+        let res = instantiate(deps.as_mut(), mock_env(), info.clone(), msg.clone()).unwrap();
+
+        // Check response attributes
+        assert_eq!(res.attributes, vec![
+            attr("method", "instantiate"),
+            attr("vault_owner", "admin"),
+            attr("max_deposit_cap", "1000000"),
+            attr("deposit_denom", "uosmo"),
+            attr("share_denom", "share"),
+            attr("max_strategy_inst", "10"),
+            attr("admin", "admin"),
+            attr("action", "update_state"),
+            attr("new_state", "Init"),
+            attr("last_statechange_bh", "12345"), // This might be the block height
+        ]);
+
+        // Check state
+        let config: Config = VAULT_CONFIG.load(&deps.storage).unwrap();
+        assert_eq!(config, Config {
+            max_deposit_cap: Uint128::new(1000000),
+            deposit_denom: "uosmo".to_string(),
+            share_denom: "share".to_string(),
+            max_strategy_inst: Uint64::new(10),
+            admin: "admin".to_string(),
+        });
+
+        let vault_owner = VAULT_OWNER.get(deps.as_ref()).unwrap();
+        assert_eq!(vault_owner, Some(Addr::unchecked("admin")));
+    }
+
+    // Initialization test #2 
+    #[test]
+    fn initialization_fails_if_admin_not_sender() {
+        let mut deps = mock_dependencies();
+
+        let msg = InstantiateMsg {
+            provault_config: Config {
+                admin: "admin".to_string(),
+                max_deposit_cap: Uint128::new(1000000),
+                deposit_denom: "uosmo".to_string(),
+                share_denom: "share".to_string(),
+                max_strategy_inst: Uint64::new(10),
+            },
+            name: "test_name".to_string(),
+            thesis: "test_thesis".to_string(),
+        };
+        let info = mock_info("not_admin", &[]);
+
+        // Try instantiating the contract
+        let err = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap_err();
+
+        // Check error
+        match err {
+            ContractError::AdminVaultOwnerMismatch {} => {}
+            _ => panic!("unexpected error: {:?}", err),
+        }
+    } 
+}
