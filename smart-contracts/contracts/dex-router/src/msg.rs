fdab6a275d597c459f152c5b162fa271b7d61894
Lars Lubkoll <11710767+lubkoll@users.noreply.github.com>
2024-06-25 10:54:37 +0200

    Reenable query of best path

--- smart-contracts/contracts/dex-router/src/msg.rs
+++ smart-contracts/contracts/dex-router/src/msg.rs
@@ -2,7 +2,7 @@ use cosmwasm_schema::{cw_serde, QueryResponses};
 use cosmwasm_std::Uint128;
 #[cfg(not(target_arch = "wasm32"))]
 use cw_asset::AssetInfo;
-use cw_asset::AssetInfoUnchecked;
+use cw_asset::{Asset, AssetInfoUnchecked};
 use osmosis_std::types::osmosis::poolmanager::v1beta1::SwapAmountInRoute;
 
 #[cw_serde]
@@ -34,11 +34,11 @@ pub struct BestPathForPairResponse {
 #[cw_serde]
 #[derive(QueryResponses)]
 pub enum QueryMsg {
-    // #[returns(Uint128)]
-    // SimulateSwapOperations {
-    //     offer: Asset,
-    //     operations: Vec<SwapAmountInRoute>,
-    // },
+    #[returns(Uint128)]
+    SimulateSwapOperations {
+        offer: Asset,
+        operations: Vec<SwapAmountInRoute>,
+    },
     /// Returns all the current path for a given (offer_asset, ask_asset) pair.
     #[returns(Vec<SwapAmountInRoute>)]
     PathsForPair {
@@ -47,13 +47,12 @@ pub enum QueryMsg {
     },
     /// finds the best path for a given (offer_asset, ask_asset) pair.
     /// if no path is found, returns None.
-    // #[returns(Option<BestPathForPairResponse>)]
-    // BestPathForPair {
-    //     offer_asset: AssetInfoUnchecked,
-    //     offer_amount: Uint128,
-    //     ask_asset: AssetInfoUnchecked,
-    //     exclude_paths: Option<Vec<u64>>,
-    // },
+    #[returns(Option<BestPathForPairResponse>)]
+    BestPathForPair {
+        offer_asset: AssetInfoUnchecked,
+        offer_amount: Uint128,
+        ask_asset: AssetInfoUnchecked,
+    },
 
     /// Returns all the assets from which there are paths to a given ask asset.
     #[returns(Vec<AssetInfo>)]
