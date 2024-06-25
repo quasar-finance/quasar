2ca6e343c8447ba0d3b2c37799c7b5e7dfb58972
Lars Lubkoll <11710767+lubkoll@users.noreply.github.com>
2024-06-25 21:08:55 +0200

    Rename dex-router to dex-router-osmosis

--- smart-contracts/contracts/dex-router-osmosis/examples/schema.rs
+++ smart-contracts/contracts/dex-router-osmosis/examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use dex_router::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use dex_router_osmosis::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
