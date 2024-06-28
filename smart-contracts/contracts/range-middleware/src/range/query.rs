7081429141b9ee31a53a7be1a4907b3a23f5924b
Laurens <32776056+LaurensKubat@users.noreply.github.com>
2024-06-28 20:36:57 +0200

    add range functions

--- smart-contracts/contracts/range-middleware/src/range/query.rs
+++ smart-contracts/contracts/range-middleware/src/range/query.rs
@@ -1,29 +1,29 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
 use cosmwasm_std::{to_json_binary, Binary, Deps, Env, StdResult};
 
-use crate::state::{NewRange, PENDING_RANGES};
+use crate::state::{RangeUpdates, PENDING_RANGES};
 
 #[cw_serde]
-#[derive(QueryResponses)]
+#[derive(cw_orch::QueryFns, QueryResponses)]
 pub enum RangeQueryMsg {
     // Get the pending ranges
-    #[returns(Vec<NewRange>)]
+    #[returns(Vec<RangeUpdates>)]
     GetQueuedRangeUpdates {},
     // Get the pending ranges for a specific contract
-    #[returns(NewRange)]
+    #[returns(RangeUpdates)]
     GetQueuedRangeUpdatesForContract { contract_address: String },
 }
 
 pub fn query_range(deps: Deps, _env: Env, query_msg: RangeQueryMsg) -> StdResult<Binary> {
     match query_msg {
-        RangeQueryMsg::GetQueuedRangeUpdates {} => get_queued_range_updates(deps),
-        RangeQueryMsg::GetQueuedRangeUpdatesForContract { contract_address } => {
-            get_queued_range_updates_for_contract(deps, contract_address)
-        }
+        RangeQueryMsg::GetQueuedRangeUpdates {} => to_json_binary(&get_queued_range_updates(deps)?),
+        RangeQueryMsg::GetQueuedRangeUpdatesForContract { contract_address } => to_json_binary(
+            &get_queued_range_updates_for_contract(deps, contract_address)?,
+        ),
     }
 }
 
-pub fn get_queued_range_updates(deps: Deps) -> StdResult<Binary> {
+pub fn get_queued_range_updates(deps: Deps) -> StdResult<Vec<RangeUpdates>> {
     let mut pending_ranges = vec![];
 
     for pending_range in
@@ -33,15 +33,15 @@ pub fn get_queued_range_updates(deps: Deps) -> StdResult<Binary> {
         pending_ranges.push(pending_range);
     }
 
-    to_json_binary(&pending_ranges)
+    Ok(pending_ranges)
 }
 
 pub fn get_queued_range_updates_for_contract(
     deps: Deps,
     contract_address: String,
-) -> StdResult<Binary> {
+) -> StdResult<RangeUpdates> {
     let pending_range =
         PENDING_RANGES.load(deps.storage, deps.api.addr_validate(&contract_address)?)?;
 
-    to_json_binary(&pending_range)
+    Ok(pending_range)
 }
