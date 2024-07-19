63c920bb0c4aab2224af76114963cf42bcf4dcee
Lars Lubkoll <11710767+lubkoll@users.noreply.github.com>
2024-07-19 20:07:25 +0200

    Add boilerplate for babylon vault

---
+++ smart-contracts/quasar/contracts/babylon-vault/src/contract.rs
@@ -0,0 +1,40 @@
+use crate::error::VaultError;
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response};
+use cw2::set_contract_version;
+
+const CONTRACT_NAME: &str = "quasar:babylon-vault";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub type VaultResult<T = Response> = Result<T, VaultError>;
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: InstantiateMsg,
+) -> VaultResult {
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+    Ok(Response::default())
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(_deps: DepsMut, _env: Env, _info: MessageInfo, _msg: ExecuteMsg) -> VaultResult {
+    Ok(Response::default())
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> VaultResult<Binary> {
+    Ok(Binary::default())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn dummy_test() {
+        assert!(true);
+    }
+}
