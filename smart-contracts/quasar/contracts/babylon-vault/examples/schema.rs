fa681837e024940f3663bd712fb71ae82fef3e04
Lars Lubkoll <11710767+lubkoll@users.noreply.github.com>
2024-07-19 20:07:25 +0200

    Add boilerplate for babylon vault

---
+++ smart-contracts/quasar/contracts/babylon-vault/examples/schema.rs
@@ -0,0 +1,10 @@
+use babylon_vault::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cosmwasm_schema::write_api;
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        query: QueryMsg,
+        execute: ExecuteMsg,
+    }
+}
