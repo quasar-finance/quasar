2ff88ebcec1d06ef2ca04e2954123dc77504434f
Lars Lubkoll <11710767+lubkoll@users.noreply.github.com>
2024-07-19 23:26:34 +0200

    Add denoms with interface contract

--- smart-contracts/quasar/contracts/babylon-vault/src/contract.rs
+++ smart-contracts/quasar/contracts/babylon-vault/src/contract.rs
@@ -1,9 +1,11 @@
 use crate::error::VaultError;
-use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
-use crate::state::OWNER;
+use crate::msg::{ExecuteMsg, InstantiateMsg, LstInfo, QueryMsg};
+use crate::state::{LSTS, OWNER};
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{to_json_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response};
+use cosmwasm_std::{
+    to_json_binary, Addr, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult,
+};
 use cw2::set_contract_version;
 
 const CONTRACT_NAME: &str = "quasar:babylon-vault";
@@ -31,7 +33,26 @@ pub fn instantiate(
 pub fn execute(deps: DepsMut, _env: Env, info: MessageInfo, msg: ExecuteMsg) -> VaultResult {
     match msg {
         ExecuteMsg::UpdateOwner(update) => Ok(OWNER.update(deps, info, update)?),
+        ExecuteMsg::RegisterLst { denom, interface } => register_lst(deps, info, denom, interface),
+        ExecuteMsg::UnregisterLst { denom } => unregister_lst(deps, info, denom),
         _ => Ok(Response::default()),
+    }
+}
+
+fn register_lst(deps: DepsMut, info: MessageInfo, denom: String, interface: String) -> VaultResult {
+    OWNER.assert_owner(deps.storage, &info.sender)?;
+    LSTS.save(deps.storage, denom, &deps.api.addr_validate(&interface)?)?;
+    Ok(Response::default())
+}
+
+fn unregister_lst(deps: DepsMut, info: MessageInfo, denom: String) -> VaultResult {
+    OWNER.assert_owner(deps.storage, &info.sender)?;
+    let interface = LSTS.may_load(deps.storage, denom.clone())?;
+    if interface.is_some() {
+        LSTS.remove(deps.storage, denom);
+        Ok(Response::default())
+    } else {
+        Err(VaultError::LstNotFound { denom })
     }
 }
 
@@ -39,14 +60,24 @@ pub fn execute(deps: DepsMut, _env: Env, info: MessageInfo, msg: ExecuteMsg) -> 
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> VaultResult<Binary> {
     match msg {
         QueryMsg::Owner {} => Ok(to_json_binary(&OWNER.query(deps.storage)?)?),
+        QueryMsg::Lsts {} => Ok(to_json_binary(&query_lsts(deps)?)?),
         _ => Ok(Binary::default()),
     }
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn dummy_test() {
-        assert!(true);
-    }
+fn query_lsts(deps: Deps) -> StdResult<Vec<LstInfo>> {
+    let lsts: StdResult<Vec<(String, Addr)>> = LSTS
+        .range(deps.storage, None, None, cosmwasm_std::Order::Ascending)
+        .collect();
+    let lsts = lsts?;
+    let infos: Vec<LstInfo> = lsts
+        .into_iter()
+        .map(|(denom, interface)| -> LstInfo {
+            LstInfo {
+                denom,
+                interface: interface.to_string(),
+            }
+        })
+        .collect();
+    Ok(infos)
 }
