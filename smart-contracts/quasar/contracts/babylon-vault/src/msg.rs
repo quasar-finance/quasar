8ab756dca55dac44a2b7c53c906497671d896e46
lubkoll <11710767+lubkoll@users.noreply.github.com>
2024-07-24 08:53:15 +0000

    Add owner (#704)

--- smart-contracts/quasar/contracts/babylon-vault/src/msg.rs
+++ smart-contracts/quasar/contracts/babylon-vault/src/msg.rs
@@ -1,8 +1,11 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
 use cosmwasm_std::{Timestamp, Uint128};
+use mars_owner::{OwnerResponse, OwnerUpdate};
 
 #[cw_serde]
-pub struct InstantiateMsg {}
+pub struct InstantiateMsg {
+    pub owner: String,
+}
 
 #[cw_serde]
 pub enum ExecuteMsg {
@@ -12,6 +15,7 @@ pub enum ExecuteMsg {
     Claim {},
     // owner methods
     RegisterLst { denom: String },
+    UpdateOwner(OwnerUpdate),
 }
 
 #[cw_serde]
@@ -29,4 +33,6 @@ pub enum QueryMsg {
     Claimable { address: String },
     #[returns(Uint128)]
     BalanceInUnderlying {},
+    #[returns(OwnerResponse)]
+    Owner {},
 }
