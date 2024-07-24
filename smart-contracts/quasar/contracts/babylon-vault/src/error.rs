e604a46d307daa5f5474b2ec7b260113940c3e91
lubkoll <11710767+lubkoll@users.noreply.github.com>
2024-07-24 08:53:15 +0000

    Add owner (#704)

--- smart-contracts/quasar/contracts/babylon-vault/src/error.rs
+++ smart-contracts/quasar/contracts/babylon-vault/src/error.rs
@@ -1,8 +1,12 @@
 use cosmwasm_std::StdError;
+use mars_owner::OwnerError;
 use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq)]
 pub enum VaultError {
     #[error("{0}")]
     Std(#[from] StdError),
+
+    #[error("{0}")]
+    Owner(#[from] OwnerError),
 }
