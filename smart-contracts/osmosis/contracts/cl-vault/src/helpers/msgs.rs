047383a2c8981053b9a5eca8930d6f9398bb660e
Lars Lubkoll <11710767+lubkoll@users.noreply.github.com>
2024-08-16 13:14:15 +0200

    Remove calculate_swap_amount

--- smart-contracts/osmosis/contracts/cl-vault/src/helpers/msgs.rs
+++ smart-contracts/osmosis/contracts/cl-vault/src/helpers/msgs.rs
@@ -1,11 +1,7 @@
 use crate::{state::POSITION, ContractError};
-use cosmwasm_std::{
-    attr, to_json_binary, Addr, Attribute, BankMsg, Coin, CosmosMsg, Deps, Env, Uint128, WasmMsg,
-};
-use dex_router_osmosis::msg::ExecuteMsg as DexRouterExecuteMsg;
-use osmosis_std::types::osmosis::{
-    concentratedliquidity::v1beta1::{MsgCollectIncentives, MsgCollectSpreadRewards},
-    poolmanager::v1beta1::SwapAmountInRoute,
+use cosmwasm_std::{attr, Addr, Attribute, BankMsg, Coin, Deps, Env, Uint128};
+use osmosis_std::types::osmosis::concentratedliquidity::v1beta1::{
+    MsgCollectIncentives, MsgCollectSpreadRewards,
 };
 
 pub fn refund_bank_msg(
@@ -42,66 +38,6 @@ pub fn refund_bank_msg(
     Ok(result)
 }
 
-/// Swaps
-pub fn swap_msg(
-    sender: Addr,
-    pool_id: u64,
-    token_in: Coin,
-    min_receive: Coin,
-    forced_swap_route: Option<Vec<SwapAmountInRoute>>,
-    dex_router: Option<Addr>,
-) -> Result<CosmosMsg, ContractError> {
-    if let Some(dex_router) = dex_router {
-        cw_dex_execute_swap_operations_msg(dex_router, forced_swap_route, token_in, min_receive)
-    } else {
-        let pool_route = SwapAmountInRoute {
-            pool_id,
-            token_out_denom: min_receive.denom,
-        };
-        Ok(osmosis_swap_exact_amount_in_msg(
-            sender,
-            pool_route,
-            token_in,
-            min_receive.amount,
-        ))
-    }
-}
-
-fn osmosis_swap_exact_amount_in_msg(
-    sender: Addr,
-    pool_route: SwapAmountInRoute,
-    token_in: Coin,
-    token_out_min_amount: Uint128,
-) -> CosmosMsg {
-    osmosis_std::types::osmosis::poolmanager::v1beta1::MsgSwapExactAmountIn {
-        sender: sender.to_string(),
-        routes: vec![pool_route],
-        token_in: Some(token_in.into()),
-        token_out_min_amount: token_out_min_amount.to_string(),
-    }
-    .into()
-}
-
-fn cw_dex_execute_swap_operations_msg(
-    dex_router_address: Addr,
-    path: Option<Vec<SwapAmountInRoute>>,
-    token_in: Coin,
-    min_receive: Coin,
-) -> Result<CosmosMsg, ContractError> {
-    let swap_msg: CosmosMsg = WasmMsg::Execute {
-        contract_addr: dex_router_address.to_string(),
-        msg: to_json_binary(&DexRouterExecuteMsg::Swap {
-            path,
-            out_denom: min_receive.denom,
-            minimum_receive: Some(min_receive.amount),
-        })?,
-        funds: vec![token_in],
-    }
-    .into();
-
-    Ok(swap_msg)
-}
-
 pub fn collect_incentives_msg(deps: Deps, env: Env) -> Result<MsgCollectIncentives, ContractError> {
     let position = POSITION.load(deps.storage)?;
     Ok(MsgCollectIncentives {
@@ -119,71 +55,4 @@ pub fn collect_spread_rewards_msg(
         position_ids: vec![position.position_id],
         sender: env.contract.address.into(),
     })
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use cosmwasm_std::{
-        coin,
-        testing::{mock_dependencies_with_balance, mock_env},
-        Coin, CosmosMsg, Uint128,
-    };
-
-    use crate::state::{PoolConfig, POOL_CONFIG};
-
-    fn mock_pool_config() -> PoolConfig {
-        PoolConfig {
-            pool_id: 1,
-            token0: "token0".to_string(),
-            token1: "token1".to_string(),
-        }
-    }
-
-    #[test]
-    fn test_proper_swap() {
-        let mut deps = mock_dependencies_with_balance(&[Coin {
-            denom: "token0".to_string(),
-            amount: Uint128::new(1000),
-        }]);
-        let deps_mut = deps.as_mut();
-
-        let env = mock_env();
-
-        let token_in_amount = Uint128::new(100);
-        let token_in_denom = "token0".to_string();
-        let token_out_min_amount = Uint128::new(100);
-        let token_out_denom = "token1".to_string();
-
-        POOL_CONFIG
-            .save(deps_mut.storage, &mock_pool_config())
-            .unwrap();
-        let result = swap_msg(
-            env.contract.address.clone(),
-            1,
-            coin(token_in_amount.into(), token_in_denom),
-            coin(token_out_min_amount.into(), token_out_denom),
-            None,
-            None,
-        )
-        .unwrap();
-
-        if let CosmosMsg::Stargate { type_url: _, value } = result {
-            let msg_swap =
-                osmosis_std::types::osmosis::poolmanager::v1beta1::MsgSwapExactAmountIn::try_from(
-                    value,
-                )
-                .unwrap();
-
-            assert!(msg_swap.sender == env.contract.address);
-            assert!(msg_swap.routes.len() == 1);
-            assert!(msg_swap.routes[0].pool_id == 1);
-            assert!(msg_swap.routes[0].token_out_denom == *"token1");
-            assert!(msg_swap.token_in.clone().unwrap().denom == *"token0");
-            assert!(msg_swap.token_in.unwrap().amount == *"100");
-            assert!(token_out_min_amount.to_string() == *"100");
-        } else {
-            panic!("Unexpected message type: {:?}", result);
-        }
-    }
 }
--- smart-contracts/osmosis/contracts/cl-vault/src/vault/swap.rs
+++ smart-contracts/osmosis/contracts/cl-vault/src/vault/swap.rs
@@ -1,20 +1,16 @@
-use cosmwasm_std::{coin, Addr, Coin, CosmosMsg, Decimal, DepsMut, MessageInfo, Response, Uint128};
+use cosmwasm_std::{
+    coin, to_json_binary, Addr, CheckedMultiplyFractionError, Coin, CosmosMsg, Decimal, DepsMut,
+    MessageInfo, Response, Uint128, WasmMsg,
+};
+use dex_router_osmosis::msg::ExecuteMsg as DexRouterExecuteMsg;
 use osmosis_std::types::osmosis::poolmanager::v1beta1::SwapAmountInRoute;
 
 use crate::{
     error::assert_range_admin,
-    helpers::msgs::swap_msg,
     msg::SwapOperation,
-    state::{PoolConfig, DEX_ROUTER, POOL_CONFIG, VAULT_CONFIG},
+    state::{DEX_ROUTER, POOL_CONFIG, VAULT_CONFIG},
     ContractError,
 };
-
-/// SwapCalculationResult holds the result of a swap calculation
-pub struct SwapCalculationResult {
-    pub swap_msg: CosmosMsg,
-    pub token_in: Coin,
-    pub token_out_min_amount: Uint128,
-}
 
 pub fn execute_swap_non_vault_funds(
     deps: DepsMut,
@@ -22,7 +18,6 @@ pub fn execute_swap_non_vault_funds(
     info: MessageInfo,
     swap_operations: Vec<SwapOperation>,
 ) -> Result<Response, ContractError> {
-    // validate auto compound admin as the purpose of swaps are mainly around autocompound non-vault assets into assets that can be actually compounded.
     assert_range_admin(deps.storage, &info.sender)?;
 
     let vault_config = VAULT_CONFIG.load(deps.storage)?;
@@ -39,12 +34,10 @@ pub fn execute_swap_non_vault_funds(
         let pool_token_0 = pool_config.token0.clone();
         let pool_token_1 = pool_config.token1.clone();
 
-        // Assert that no BASE_DENOM or QUOTE_DENOM is trying to be swapped as token_in
         if token_in_denom == pool_token_0 || token_in_denom == pool_token_1 {
             return Err(ContractError::InvalidSwapAssets {});
         }
 
-        // Throw an Error if contract balance for the wanted denom is 0
         let balance_in_contract = deps
             .querier
             .query_balance(
@@ -57,13 +50,11 @@ pub fn execute_swap_non_vault_funds(
             return Err(ContractError::InsufficientFunds {});
         }
 
-        // TODO_FUTURE: We could be swapping into the actual vault balance so we could prepend_swap() the autocompound entrypoint.
         let part_0_amount = balance_in_contract.checked_div(Uint128::new(2))?;
         let part_1_amount = balance_in_contract
             .checked_add(Uint128::new(1))?
             .checked_div(Uint128::new(2))?;
 
-        // TODO_FUTURE: We should be passing the max_slippage from outside as we do during ModifyRange
         let token_out_min_amount_0 =
             part_0_amount.checked_mul_floor(vault_config.swap_max_slippage)?;
         let token_out_min_amount_1 =
@@ -94,34 +85,87 @@ pub fn execute_swap_non_vault_funds(
         .add_attribute("action", "swap_non_vault_funds"))
 }
 
-#[allow(clippy::too_many_arguments)]
-pub fn calculate_swap_amount(
-    contract_address: Addr,
-    pool_config: PoolConfig,
-    token_in: Coin,
-    out_denom: String,
-    max_slippage: Decimal,
-    forced_swap_route: Option<Vec<SwapAmountInRoute>>,
+pub fn estimate_swap_min_out_amount(
+    in_amount: Uint128,
     price: Decimal,
-    dex_router: Option<Addr>,
-) -> Result<SwapCalculationResult, ContractError> {
-    let token_out_min_amount = token_in
-        .amount
+    slippage_factor: Decimal,
+) -> Result<Uint128, CheckedMultiplyFractionError> {
+    in_amount
         .checked_mul_floor(price)?
-        .checked_mul_floor(max_slippage)?;
+        .checked_mul_floor(slippage_factor)
+}
 
-    let swap_msg = swap_msg(
-        contract_address,
-        pool_config.pool_id,
-        token_in.clone(),
-        coin(token_out_min_amount.into(), out_denom.clone()),
-        forced_swap_route,
-        dex_router,
-    )?;
+pub fn swap_msg(
+    sender: Addr,
+    pool_id: u64,
+    token_in: Coin,
+    min_receive: Coin,
+    forced_swap_route: Option<Vec<SwapAmountInRoute>>,
+    dex_router: Option<Addr>,
+) -> Result<CosmosMsg, ContractError> {
+    if let Some(dex_router) = dex_router {
+        cw_dex_execute_swap_operations_msg(dex_router, forced_swap_route, token_in, min_receive)
+    } else {
+        let pool_route = SwapAmountInRoute {
+            pool_id,
+            token_out_denom: min_receive.denom,
+        };
+        Ok(osmosis_swap_exact_amount_in_msg(
+            sender,
+            pool_route,
+            token_in,
+            min_receive.amount,
+        ))
+    }
+}
 
-    Ok(SwapCalculationResult {
-        swap_msg,
-        token_in,
-        token_out_min_amount,
-    })
+fn osmosis_swap_exact_amount_in_msg(
+    sender: Addr,
+    pool_route: SwapAmountInRoute,
+    token_in: Coin,
+    token_out_min_amount: Uint128,
+) -> CosmosMsg {
+    osmosis_std::types::osmosis::poolmanager::v1beta1::MsgSwapExactAmountIn {
+        sender: sender.to_string(),
+        routes: vec![pool_route],
+        token_in: Some(token_in.into()),
+        token_out_min_amount: token_out_min_amount.to_string(),
+    }
+    .into()
+}
+
+fn cw_dex_execute_swap_operations_msg(
+    dex_router_address: Addr,
+    path: Option<Vec<SwapAmountInRoute>>,
+    token_in: Coin,
+    min_receive: Coin,
+) -> Result<CosmosMsg, ContractError> {
+    let swap_msg: CosmosMsg = WasmMsg::Execute {
+        contract_addr: dex_router_address.to_string(),
+        msg: to_json_binary(&DexRouterExecuteMsg::Swap {
+            path,
+            out_denom: min_receive.denom,
+            minimum_receive: Some(min_receive.amount),
+        })?,
+        funds: vec![token_in],
+    }
+    .into();
+
+    Ok(swap_msg)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cosmwasm_std::Uint128;
+
+    #[test]
+    fn test_estimate_swap_min_out_amount() {
+        let in_amount = Uint128::from(50u64);
+        let price = Decimal::percent(300);
+        let max_slippage = Decimal::percent(50);
+
+        let out_amount = estimate_swap_min_out_amount(in_amount, price, max_slippage).unwrap();
+        assert_eq!(out_amount, Uint128::from(75u64));
+    }
 }
