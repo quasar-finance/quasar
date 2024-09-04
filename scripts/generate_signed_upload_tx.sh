10359245d40a087e44bf71e8124a655d231a79d1
Lars Lubkoll <11710767+lubkoll@users.noreply.github.com>
2024-09-04 16:55:29 +0000

    Get multisig address from key name

--- scripts/generate_signed_upload_tx.sh
+++ scripts/generate_signed_upload_tx.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: bash generate_signed_upload_tx.sh <WASM_FILE> <DEPLOYER>"
+set -e
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: bash generate_signed_upload_tx.sh <WASM_FILE> <DEPLOYER> <MULTISIG>"
+    echo "DEPLOYER and MULTISIG refer to the names of keys that are registered with osmosisd"
     exit 1
 fi
 
 WASM_FILE=$1
 DEPLOYER=$2
+MULTISIG=$3
 
 FEES=1000000uosmo
 NODE=https://rpc.osmosis.zone:443
-MULTISIG=osmo1vxq5h3encfyguulqeh26l8dlw9lavl3e2zw7n8
+MULTISIG_ADDRESS=$(osmosisd keys show ${MULTISIG} | grep address | sed "s/- address: //g")
 CHAIN=osmosis-1
 
 set -e
 
-osmosisd tx wasm store ${WASM_FILE} --from contract-upload --gas 25000000 --fees ${FEES} --chain-id ${CHAIN} --node ${NODE} --generate-only > tx.json
-osmosisd tx sign tx.json --multisig=${MULTISIG} --sign-mode amino-json --chain-id ${CHAIN} --node ${NODE} --from ${DEPLOYER} --output-document ${DEPLOYER}-signed-tx.json
+osmosisd tx wasm store ${WASM_FILE} --from ${MULTISIG} --gas 25000000 --fees ${FEES} --chain-id ${CHAIN} --node ${NODE} --generate-only > tx.json
+osmosisd tx sign tx.json --multisig=${MULTISIG_ADDRESS} --sign-mode amino-json --chain-id ${CHAIN} --node ${NODE} --from ${DEPLOYER} --output-document ${DEPLOYER}-signed-tx.json
