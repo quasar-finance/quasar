143aad179c7c03dc7419b2c0df475b4227c1dd1f
Arham Chordia <chordia.1@iitj.ac.in>
2024-09-05 15:57:05 +0530

    E2e cleanup.

---
+++ tests/e2e/helpers/gov.go
@@ -0,0 +1,48 @@
+package helpers
+
+import (
+	"context"
+	govv1beta1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
+	"strconv"
+	"testing"
+
+	"github.com/strangelove-ventures/interchaintest/v8/chain/cosmos"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
+)
+
+// Modified from ictest
+func VoteOnProposalAllValidators(ctx context.Context, c *cosmos.CosmosChain, proposalID string, vote string) error {
+	var eg errgroup.Group
+	valKey := "validator"
+	for _, n := range c.Nodes() {
+		if n.Validator {
+			n := n
+			eg.Go(func() error {
+				// gas-adjustment was using 1.3 default instead of the setup's 2.0+ for some reason.
+				// return n.VoteOnProposal(ctx, valKey, proposalID, vote)
+
+				_, err := n.ExecTx(ctx, valKey,
+					"gov", "vote",
+					proposalID, vote, "--gas", "auto", "--gas-adjustment", "2.0",
+				)
+				return err
+			})
+		}
+	}
+	return eg.Wait()
+}
+
+func ValidatorVote(t *testing.T, ctx context.Context, chain *cosmos.CosmosChain, proposalID string, searchHeightDelta uint64) {
+	err := VoteOnProposalAllValidators(ctx, chain, proposalID, cosmos.ProposalVoteYes)
+	require.NoError(t, err, "failed to vote on proposal")
+
+	height, err := chain.Height(ctx)
+	require.NoError(t, err, "failed to get height")
+
+	proposalIDInt, err := strconv.ParseUint(proposalID, 10, 64)
+	require.NoError(t, err, "failed to parse proposal id")
+
+	_, err = cosmos.PollForProposalStatus(ctx, chain, height, height+int64(searchHeightDelta), proposalIDInt, govv1beta1.StatusPassed)
+	require.NoError(t, err, "proposal status did not change to passed in expected number of blocks")
+}
