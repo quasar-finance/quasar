491a1e26e30a23dba68450d59695c493c9609ad7
Lars Lubkoll <11710767+lubkoll@users.noreply.github.com>
2024-09-06 19:24:03 +0000

    Pass multisig key name to upload-script

--- scripts/upload_through_multisig.sh
+++ scripts/upload_through_multisig.sh
@@ -2,16 +2,17 @@
 
 set -e
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ]; then
     echo "Too many arguments. Please provide a string containing the names of the json files of the signed transactions."
-    echo "Usage: bash upload_through_multisig.sh \"<SIGNED_TX_1> <SIGNED_TX_2>\""
+    echo "Usage: bash upload_through_multisig.sh <MULTISIG> \"<SIGNED_TX_1> <SIGNED_TX_2>\""
     exit 1
 fi
 
-SIGNED_TXS=$1
+MULTISIG=$1
+SIGNED_TXS=$2
 NODE=https://rpc.osmosis.zone:443
 CHAIN=osmosis-1
 
-osmosisd tx multisign tx.json contract-upload ${SIGNED_TXS} --chain-id ${CHAIN} --node ${NODE} --from contract-upload --output-document tx_ms.json
+osmosisd tx multisign tx.json ${MULTISIG} ${SIGNED_TXS} --chain-id ${CHAIN} --node ${NODE} --from ${MULTISIG} --output-document tx_ms.json
 osmosisd tx broadcast tx_ms.json --chain-id ${CHAIN} --node ${NODE}
 rm tx_ms.json
